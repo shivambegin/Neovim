350b42cc1271f0e88bb4306377f96267f8edf739
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-01-13 20:51:13 +0600

    update telescope keybindings

--- lua/amahmod/telescope.lua
+++ lua/amahmod/telescope.lua
@@ -12,11 +12,11 @@ telescope.setup {
 
     mappings = {
       i = {
-        ['<C-n>'] = actions.cycle_history_next,
-        ['<C-p>'] = actions.cycle_history_prev,
+        ['<C-k>'] = actions.cycle_history_next,
+        ['<C-j>'] = actions.cycle_history_prev,
 
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-n>'] = actions.move_selection_next,
+        ['<C-p>'] = actions.move_selection_previous,
 
         ['<C-c>'] = actions.close,
 
