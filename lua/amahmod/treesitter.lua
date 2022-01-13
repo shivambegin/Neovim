8865dd4067e667169c67cad2022081e27c3edaef
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-01-13 21:44:53 +0600

    treesitter: disable highlight current scope

--- lua/amahmod/treesitter.lua
+++ lua/amahmod/treesitter.lua
@@ -26,7 +26,7 @@ configs.setup {
   },
   refactor = {
     highlight_definitions = {enable = true},
-    highlight_current_scope = {enable = true},
+    highlight_current_scope = {enable = false},
     navigation = {
       enable = true,
       keymaps = {
