66b7416f9bfa99971c40fda2003d51b65c0ec604
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-01-13 17:26:28 +0600

    add gitsigns

--- lua/amahmod/plugins.lua
+++ lua/amahmod/plugins.lua
@@ -78,6 +78,8 @@ return packer.startup(function(use)
   use "nvim-treesitter/nvim-treesitter-textobjects"
   use "nvim-treesitter/nvim-treesitter-refactor"
 
+-- Git
+  use "lewis6991/gitsigns.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
