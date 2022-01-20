82e3f28db5a4c75d7b5402235b94baa8b2aca71d
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-01-20 20:57:38 +0600

    Disable formation for all language server but eslint

--- lua/amahmod/lsp/handlers.lua
+++ lua/amahmod/lsp/handlers.lua
@@ -63,7 +63,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == 'tsserver' then client.resolved_capabilities.document_formatting = false end
+  client.resolved_capabilities.document_formatting = false
 
   if client.name == 'eslint' then client.resolved_capabilities.document_formatting = true end
 
