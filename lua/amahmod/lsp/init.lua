06232a96adb85a142b10e1f1ebf560f61820a577
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-01-31 13:53:39 +0600

    clear config

--- lua/amahmod/lsp/init.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, _ = pcall(require, 'lspconfig')
-if not status_ok then return end
-
-require('amahmod.lsp.lsp-installer')
-require('amahmod.lsp.handlers').setup()
-require('amahmod.lsp.null-ls')
--- lua/amahmod/lsp/settings/pyright.lua
+++
@@ -1,1 +0,0 @@
-return {settings = {python = {analysis = {typeCheckingMode = 'off'}}}}
