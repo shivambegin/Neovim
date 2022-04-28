2909ea242d830caa963b40923b9bf34b270c6d40
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-04-28 14:55:06 +0600

    plugins: refactor telescope configuration

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,71 +1,96 @@
 local M = {}
 
 function M.config()
-
   local status_ok, telescope = pcall(require, 'telescope')
   if not status_ok then return end
-  local action_set = require('telescope.actions.set')
 
-  telescope.setup(
-    {
-      pickers = {
-        find_files = {
-          hidden = true,
-          attach_mappings = function(_)
-            action_set.select:enhance({post = function() vim.cmd(':normal! zx') end})
-            return true
-          end,
+  local actions = require 'telescope.actions'
+
+  local notify_present, _ = pcall(require, 'notify')
+  if notify_present then telescope.load_extension 'notify' end
+
+  telescope.setup {
+    defaults = {
+
+      prompt_prefix = ' ',
+      selection_caret = '❯ ',
+      path_display = {'truncate'},
+      selection_strategy = 'reset',
+      sorting_strategy = 'ascending',
+      layout_strategy = 'horizontal',
+      layout_config = {
+        horizontal = {prompt_position = 'top', preview_width = 0.55, results_width = 0.8},
+        vertical = {mirror = false},
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
+
+      mappings = {
+        i = {
+          ['<C-k>'] = actions.cycle_history_next,
+          ['<C-j>'] = actions.cycle_history_prev,
+
+          ['<C-n>'] = actions.move_selection_next,
+          ['<C-p>'] = actions.move_selection_previous,
+
+          ['<C-c>'] = actions.close,
+
+          ['<Down>'] = actions.move_selection_next,
+          ['<Up>'] = actions.move_selection_previous,
+
+          ['<CR>'] = actions.select_default,
+          ['<C-x>'] = actions.select_horizontal,
+          ['<C-v>'] = actions.select_vertical,
+          ['<C-t>'] = actions.select_tab,
+
+          ['<C-u>'] = actions.preview_scrolling_up,
+          ['<C-d>'] = actions.preview_scrolling_down,
+
+          ['<PageUp>'] = actions.results_scrolling_up,
+          ['<PageDown>'] = actions.results_scrolling_down,
+
+          ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+          ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+          ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+          ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+          ['<C-l>'] = actions.complete_tag,
+        },
+
+        n = {
+          ['<esc>'] = actions.close,
+          ['<CR>'] = actions.select_default,
+          ['<C-x>'] = actions.select_horizontal,
+          ['<C-v>'] = actions.select_vertical,
+          ['<C-t>'] = actions.select_tab,
+
+          ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+          ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+          ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+          ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+
+          ['j'] = actions.move_selection_next,
+          ['k'] = actions.move_selection_previous,
+          ['H'] = actions.move_to_top,
+          ['M'] = actions.move_to_middle,
+          ['L'] = actions.move_to_bottom,
+
+          ['<Down>'] = actions.move_selection_next,
+          ['<Up>'] = actions.move_selection_previous,
+          ['gg'] = actions.move_to_top,
+          ['G'] = actions.move_to_bottom,
+
+          ['<C-u>'] = actions.preview_scrolling_up,
+          ['<C-d>'] = actions.preview_scrolling_down,
+
+          ['<PageUp>'] = actions.results_scrolling_up,
+          ['<PageDown>'] = actions.results_scrolling_down,
         },
       },
-      defaults = {
-        prompt_prefix = '🔍 ',
-        selection_caret = '> ',
-        entry_prefix = '  ',
-        initial_mode = 'insert',
-        selection_strategy = 'reset',
-        sorting_strategy = 'descending',
-        layout_strategy = 'horizontal',
-        file_sorter = require('telescope.sorters').get_generic_sorter,
-        file_ignore_patterns = {'.git/', 'node_modules', '__snapshots_-'},
-        generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-        winblend = 0,
-        border = {},
-        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-        color_devicons = true,
-        use_less = true,
-        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
-        file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-        grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-        -- Developer configurations: Not meant for general override
-        buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
-      },
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
-        },
-        file_browser = {
-          theme = 'ivy',
-          mappings = {
-            ['i'] = {
-              -- your custom insert mode mappings
-            },
-            ['n'] = {
-              -- your custom normal mode mappings
-            },
-          },
-        },
-        project = {
-          base_dirs = {'~/workspace', '~/workspace/client', '~/.config/nvim'},
-          max_depth = 4,
-          hidden_files = true,
-        },
-      },
-    }
-  )
+    },
+    pickers = {},
+    extensions = {},
+  }
 end
 
 return M
