5e6025c8af9806ef71391e2753206fcbc56d6af2
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-04-28 13:44:04 +0600

    feat: add custom ui for input and telescope

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ require 'core.plugins'
 require 'core.autocommands'
 require 'core.mappings'
 require 'core.colorscheme'
+
+local status_ok, ui = pcall(require, 'core.ui')
+if status_ok then ui.init() end
