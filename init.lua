5fa8a889bc412b0e11111dd5637ae376d6d2f711
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-05-16 21:31:58 +0600

    perf: add impatient.nvim to reduce load time

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+local impatient_ok, impatient = pcall(require, 'impatient')
+if impatient_ok then impatient.enable_profile() end
+
 require 'core.options'
 require 'core.plugins'
 require 'core.autocommands'
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -4,6 +4,9 @@ local plugins = {
   -- Plugin manager
   ['wbthomason/packer.nvim'] = {},
 
+  -- Optimiser
+  ['lewis6991/impatient.nvim'] = {},
+
   -- An implementation of the Popup API from vim in Neovim
   ['nvim-lua/popup.nvim'] = {},
 
