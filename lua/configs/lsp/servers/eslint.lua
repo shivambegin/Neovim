ebf1b0ddca0822242f4a609ea653eea0783efade
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-06-18 17:52:42 +0600

    lsp: enable Eslint on svelte filetype

--- lua/configs/lsp/servers/eslint.lua
+++ lua/configs/lsp/servers/eslint.lua
@@ -2,7 +2,22 @@ local M = {}
 M.setup = function(on_attach, capabilities)
   local lspconfig = require 'lspconfig'
 
-  lspconfig.eslint.setup {on_attach = on_attach, flags = {debounce_text_changes = 150}, capabilities = capabilities}
+  lspconfig.eslint.setup {
+    on_attach = on_attach,
+    flags = {debounce_text_changes = 150},
+    capabilities = capabilities,
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+      'vue',
+      'svelte',
+    },
+
+  }
 end
 
 return M
