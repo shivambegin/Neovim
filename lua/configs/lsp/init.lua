2a40f5ec9b87db35148aa5206aa116e78df7fc77
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-06-29 19:35:14 +0600

    refactor: go to definition in split window

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -18,6 +18,36 @@ local function lsp_highlight_document(client)
   end
 end
 
+local function goto_definition(split_cmd)
+  local util = vim.lsp.util
+  local log = require('vim.lsp.log')
+  local api = vim.api
+
+  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
+  local handler = function(_, result, ctx)
+    if result == nil or vim.tbl_isempty(result) then
+      local _ = log.info() and log.info(ctx.method, 'No location found')
+      return nil
+    end
+
+    if split_cmd then vim.cmd(split_cmd) end
+
+    if vim.tbl_islist(result) then
+      util.jump_to_location(result[1])
+
+      if #result > 1 then
+        util.set_qflist(util.locations_to_items(result))
+        api.nvim_command('copen')
+        api.nvim_command('wincmd p')
+      end
+    else
+      util.jump_to_location(result)
+    end
+  end
+
+  return handler
+end
+
 local function set_keymappings(client, bufnr)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {desc = 'Hover symbol details', buffer = bufnr})
   vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, {desc = 'LSP code action', buffer = bufnr})
@@ -116,6 +146,7 @@ function M.config()
     vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'})
 
     vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded'})
+    vim.lsp.handlers['textDocument/definition'] = goto_definition('split')
 
   end
 end
