07926ccc9aba2e94e9c4bab33b16ab2c58f2712d
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-06-29 18:47:30 +0600

    refactor: use <C-n>, <C-p> to select next,previous list item

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -61,8 +61,8 @@ function M.config()
       end,
     },
     mapping = {
-      ['<C-k>'] = cmp.mapping.select_prev_item(),
-      ['<C-j>'] = cmp.mapping.select_next_item(),
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), {'i', 'c'}),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), {'i', 'c'}),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
@@ -70,8 +70,8 @@ function M.config()
       ['<C-e>'] = cmp.mapping {i = cmp.mapping.abort(), c = cmp.mapping.close()},
       -- Accept currently selected item. If none selected, `select` first item.
       -- Set `select` to `false` to only confirm explicitly selected items.
-      ['<CR>'] = cmp.mapping.confirm {select = true},
-      ['<C-n>'] = cmp.mapping(
+      ['<CR>'] = cmp.mapping.confirm {select = false},
+      ['<C-j>'] = cmp.mapping(
         function(fallback)
           local copilot_keys = vim.fn['copilot#Accept']()
           if copilot_keys ~= '' then
--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -28,11 +28,11 @@ function M.config()
 
       mappings = {
         i = {
-          ['<C-n>'] = actions.cycle_history_next,
-          ['<C-p>'] = actions.cycle_history_prev,
+          ['<C-j>'] = actions.cycle_history_next,
+          ['<C-k>'] = actions.cycle_history_prev,
 
-          ['<C-j>'] = actions.move_selection_next,
-          ['<C-k>'] = actions.move_selection_previous,
+          ['<C-n>'] = actions.move_selection_next,
+          ['<C-p>'] = actions.move_selection_previous,
 
           ['<C-c>'] = actions.close,
 
