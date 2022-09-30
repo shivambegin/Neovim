341f6a34bc265a81a2fb8c104a8f4563cd4aaadf
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-10-01 00:14:11 +0600

    refactor: nvim-treesitter-context

--- lua/configs/nvim-treesitter-context.lua
+++ lua/configs/nvim-treesitter-context.lua
@@ -1,13 +1,10 @@
 require('treesitter-context').setup {
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-    min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+    enable = true,
+    throttle = true,
+    max_lines = 0,
+    -- trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    -- min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
         default = {
             'class',
             'function',
@@ -18,8 +15,6 @@ require('treesitter-context').setup {
             'switch',
             'case',
         },
-        -- Patterns for specific filetypes
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
         tex = {
             'chapter',
             'section',
@@ -30,6 +25,8 @@ require('treesitter-context').setup {
             'impl_item',
             'struct',
             'enum',
+            'loop_expression',
+            'impl_item',
         },
         scala = {
             'object_definition',
@@ -58,20 +55,11 @@ require('treesitter-context').setup {
         yaml = {
             'block_mapping_pair',
         },
+        typescript = {
+            'class_declaration',
+            'abstract_class_declaration',
+            'else_clause',
+        },
     },
-    exact_patterns = {
-        -- Example for a specific filetype with Lua patterns
-        -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-        -- exactly match "impl_item" only)
-        -- rust = true,
-    },
-
-    -- [!] The options below are exposed but shouldn't require your attention,
-    --     you can safely ignore them.
-
-    zindex = 20, -- The Z-index of the context window
-    mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-    -- Separator between context and content. Should be a single character string, like '-'.
-    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-    separator = nil,
+    exact_patterns = {},
 }
