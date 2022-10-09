57517d25ce19c3bb5bb92c6789025ad59479be41
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-10-09 22:22:59 +0600

    mini.ai disable indentscope

--- lua/configs/mini.lua
+++ lua/configs/mini.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 
 require('mini.ai').setup() -- create textobjects
 require('mini.bufremove').setup() -- remove buffer without affecting layout
-require('mini.indentscope').setup() -- Visualize and operate on indent scope
+-- require('mini.indentscope').setup() -- Visualize and operate on indent scope
 require('mini.surround').setup() -- Fast and feature-rich surround plugin
 require('mini.align').setup() -- aling text
 require('mini.comment').setup() -- comment / uncomment
