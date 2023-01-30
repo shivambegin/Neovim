7d3ba872769c8f8a16cb65b3f1f8f209a1605bb6
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-01-30 09:30:09 +0600

    remove bufferline plugin

--- lua/amahmod/plugins/ui.lua
+++ lua/amahmod/plugins/ui.lua
@@ -39,35 +39,6 @@ return {
         end,
     },
 
-    -- bufferline
-    {
-        'akinsho/nvim-bufferline.lua',
-        event = 'BufAdd',
-        opts = {
-            options = {
-                diagnostics = 'nvim_lsp',
-                always_show_bufferline = false,
-                diagnostics_indicator = function(_, _, diag)
-                    local icons =
-                        require('amahmod.configs.settings').icons.diagnostics
-                    local ret = (
-                        diag.error and icons.Error .. diag.error .. ' ' or ''
-                    )
-                        .. (diag.warning and icons.Warn .. diag.warning or '')
-                    return vim.trim(ret)
-                end,
-                offsets = {
-                    {
-                        filetype = 'neo-tree',
-                        text = 'Neo-tree',
-                        highlight = 'Directory',
-                        text_align = 'left',
-                    },
-                },
-            },
-        },
-    },
-
     -- statusline
     {
         'nvim-lualine/lualine.nvim',
