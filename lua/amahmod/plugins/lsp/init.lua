7b937155247637a0229d2b7b8d1a59cdd794d4e5
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-01-30 09:49:10 +0600

    lsp: add inc-rename plugin

--- lua/amahmod/plugins/lsp/init.lua
+++ lua/amahmod/plugins/lsp/init.lua
@@ -191,4 +191,15 @@ return {
             end
         end,
     },
+
+    -- Rename
+    {
+        'smjonas/inc-rename.nvim',
+        event = 'BufReadPre',
+        config = function()
+            require('inc_rename').setup {
+                input_buffer_type = 'dressing',
+            }
+        end,
+    },
 }
