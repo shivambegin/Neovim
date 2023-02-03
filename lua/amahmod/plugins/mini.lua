9570052d2d8bc2e4a90caf7b8a337d2210e0c469
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-02-03 20:29:45 +0600

    plugins: mini.bufremove

--- lua/amahmod/plugins/mini.lua
+++ lua/amahmod/plugins/mini.lua
@@ -72,4 +72,23 @@ return {
             require('mini.comment').setup(opts)
         end,
     },
+    {
+        'echasnovski/mini.bufremove',
+        keys = {
+            {
+                '<leader>x',
+                function()
+                    require('mini.bufremove').delete(0, false)
+                end,
+                desc = 'Delete Buffer',
+            },
+            {
+                '<leader>X',
+                function()
+                    require('mini.bufremove').delete(0, true)
+                end,
+                desc = 'Delete Buffer (Force)',
+            },
+        },
+    },
 }
