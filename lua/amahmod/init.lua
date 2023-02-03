61f9ff14dd9f060cc4342c0f1375872d8a2fbac7
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-02-03 14:12:56 +0600

    default plugins

--- lua/amahmod/init.lua
+++ lua/amahmod/init.lua
@@ -1,2 +1,11 @@
 require "amahmod.configs.options"
-require "amahmod.configs.keymaps"
+require "amahmod.configs.lazy"
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+        require "amahmod.configs.autocmds"
+        require "amahmod.configs.keymaps"
+    end
+
+})
---
+++ lua/amahmod/plugins/init.lua
@@ -0,0 +1,5 @@
+return {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+}
