92051b9d6baf6b1ff2e4d6c868560be4ad26ede2
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-02-03 20:12:57 +0600

    plugins: dressing.nvim

--- lua/amahmod/plugins/init.lua
+++ lua/amahmod/plugins/init.lua
@@ -1,5 +1,15 @@
 return {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'nvim-tree/nvim-web-devicons',
+    {
+        'stevearc/dressing.nvim',
+        event = 'VeryLazy',
+        opts = {
+            input = { relative = 'cursor' },
+            select = {
+                backend = { 'telescope', 'fzf', 'builtin', 'nui' },
+            },
+        },
+    },
 }
