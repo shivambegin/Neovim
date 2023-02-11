5913603d7875acc389004cfb8ae823c9e48504e0
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-02-11 23:40:47 +0600

    remove dressing.nvim

--- lua/amahmod/plugins/init.lua
+++ lua/amahmod/plugins/init.lua
@@ -2,14 +2,4 @@ return {
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     'nvim-tree/nvim-web-devicons',
-    {
-        'stevearc/dressing.nvim',
-        event = 'VeryLazy',
-        opts = {
-            input = { relative = 'cursor' },
-            select = {
-                backend = { 'telescope', 'fzf', 'builtin', 'nui' },
-            },
-        },
-    },
 }
