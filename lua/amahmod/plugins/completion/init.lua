fce8f567dc6d0477b8e09fc4def49177fb7adaac
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-02-17 22:44:47 +0600

    remove cmp-lsp-signture

--- lua/amahmod/plugins/completion/init.lua
+++ lua/amahmod/plugins/completion/init.lua
@@ -35,7 +35,6 @@ return {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-nvim-lsp-signature-help',
         },
         config = function()
             local cmp = require 'cmp'
@@ -60,7 +59,6 @@ return {
                     ['<C-p>'] = cmp.mapping.select_prev_item(),
                 },
                 sources = cmp.config.sources {
-                    { name = 'nvim_lsp_signature_help' },
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
                     { name = 'buffer' },
