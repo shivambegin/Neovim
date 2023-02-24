ca0e2ded98e5178f2dfc09455f3c7c5f0e792440
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-02-25 00:18:55 +0600

    feat: add inlay hints for typescript

--- lua/amahmod/plugins/lsp/init.lua
+++ lua/amahmod/plugins/lsp/init.lua
@@ -14,7 +14,32 @@ return {
                 emmet_ls = {},
                 yamlls = {},
                 dockerls = {},
-                tsserver = {},
+                tsserver = {
+                    settings = {
+                        typescript = {
+                            inlayHints = {
+                                includeInlayParameterNameHints = 'all',
+                                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                                includeInlayFunctionParameterTypeHints = true,
+                                includeInlayVariableTypeHints = true,
+                                includeInlayPropertyDeclarationTypeHints = true,
+                                includeInlayFunctionLikeReturnTypeHints = true,
+                                includeInlayEnumMemberValueHints = true,
+                            },
+                        },
+                        javascript = {
+                            inlayHints = {
+                                includeInlayParameterNameHints = 'all',
+                                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                                includeInlayFunctionParameterTypeHints = true,
+                                includeInlayVariableTypeHints = true,
+                                includeInlayPropertyDeclarationTypeHints = true,
+                                includeInlayFunctionLikeReturnTypeHints = true,
+                                includeInlayEnumMemberValueHints = true,
+                            },
+                        },
+                    },
+                },
                 gopls = {},
                 diagnosticls = {},
                 sqlls = {},
@@ -30,6 +55,10 @@ return {
                 lua_ls = {
                     settings = {
                         Lua = {
+                            hint = {
+                                enable = true,
+                                showParameterName = true,
+                            },
                             workspace = {
                                 checkThirdParty = false,
                                 library = vim.api.nvim_get_runtime_file(
@@ -136,6 +165,47 @@ return {
                 end,
             }
         end,
+    },
+    {
+        'lvimuser/lsp-inlayhints.nvim',
+        event = 'BufReadPre',
+        config = function()
+            require('lsp-inlayhints').setup {
+                show_parameter_hints = true,
+                show_parameter_hints_prefix = ' ',
+                parameter_hints_prefix = '<- ',
+                show_chaining_hints = true,
+                show_chaining_hints_prefix = ' ',
+                chaining_hints_prefix = '-> ',
+                show_type_hints = true,
+                show_type_hints_prefix = ' ',
+                type_hints_prefix = ': ',
+                max_len_align = false,
+                max_len_align_padding = 1,
+                right_align = false,
+                right_align_padding = 7,
+            }
+
+            require('amahmod.util').on_attach(function(client, buffer)
+                require('lsp-inlayhints').on_attach(client, buffer)
+            end)
+        end,
+        keys = {
+            {
+                '<leader>tih',
+                function()
+                    require('lsp-inlayhints').toggle()
+                end,
+                desc = '[T]oggle [i]nlay [h]ints',
+            },
+            {
+                '<leader>rih',
+                function()
+                    require('lsp-inlayhints').reset()
+                end,
+                desc = '[r]eset [i]nlay [h]ints',
+            },
+        },
     },
 
     -- formatters
