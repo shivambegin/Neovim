ca1e2cbffd8a65bc21b845dca066530aef8f9a77
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-03-31 14:03:17 +0600

    plugins: add vim-markdown plugin

--- lua/amahmod/plugins/extra/langs/markdown.lua
+++ lua/amahmod/plugins/extra/langs/markdown.lua
@@ -1,5 +1,9 @@
 return {
     {
+        'preservim/vim-markdown',
+        ft = { 'markdown' },
+    },
+    {
         'iamcco/markdown-preview.nvim',
         build = 'cd app && yarn install',
         ft = { 'markdown' },
