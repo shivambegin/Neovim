f744b879df08cd9e122403380a3836fcbd201eb2
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-04-25 20:59:36 +0600

    refactor: disable lsp-lense for svelte file

--- lua/amahmod/plugins/lsp/init.lua
+++ lua/amahmod/plugins/lsp/init.lua
@@ -301,7 +301,19 @@ return {
         'VidocqH/lsp-lens.nvim',
         event = 'LspAttach',
         config = function()
-            require('lsp-lens').setup {}
+            require('lsp-lens').setup {
+                enable = true,
+                include_declaration = false, -- Reference include declaration
+                sections = { -- Enable / Disable specific request
+                    definition = false,
+                    references = true,
+                    implementation = true,
+                },
+                ignore_filetype = {
+                    'prisma',
+                    'svelte',
+                },
+            }
         end,
     },
 }
