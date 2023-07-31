d370a7206c7b3f1efcb7bad3fccd5fd483ff2c27
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-07-31 18:26:15 +0600

    lsp: add bash lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -17,6 +17,7 @@ return {
                         },
                     },
                 },
+                bashls = {},
                 denols = {},
                 yamlls = {},
                 dockerls = {},
