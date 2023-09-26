f3ba3f12294be5c231843071b222e302fa266ee2
Shivam Rajput <shivamrajput769777@gmail.com>
2023-09-27 00:25:41 +0530

    auto-save

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,9 +1,10 @@
 local mason = require 'plugins.lsp.mason'
 local lspconfig = require 'plugins.lsp.lspconfig'
 local null_ls = require 'plugins.lsp.null-ls'
-
+local auto_save = require 'plugins.lsp.auto_save'
 return {
     mason,
     lspconfig,
     null_ls,
+    auto_save,
 }
