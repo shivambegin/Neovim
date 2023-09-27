7eb132ebe9b5649450f762b61c6c9d7553422d7b
Shivam Rajput <shivamrajput769777@gmail.com>
2023-09-28 05:08:07 +0530

    colorscheme

--- lua/plugins/theme/init.lua
+++ lua/plugins/theme/init.lua
@@ -4,7 +4,9 @@ return {
     -- require 'plugins.theme.kanagawa'
     -- require 'plugins.theme.everforest',
     -- require 'plugins.theme.rosepine',
-    require 'plugins.theme.synthwave84',
+    --require 'plugins.theme.synthwave84',
+    -- require 'plugins.theme.nordic',
+    require 'plugins.theme.dracula',
     require("notify").setup({
         background_colour = "#000000",
     })
