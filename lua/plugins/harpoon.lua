09e022a145a0c7a1148182b1f8d521e8864df2bb
Shivam Rajput <shivamrajputcse@gmail.com>
2023-10-01 02:25:16 +0530

    harpoon keymaps

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,77 +1,24 @@
 return {
-    {
-        'ThePrimeagen/harpoon',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        event = {
-            'BufReadPost',
-            'BufNewFile',
-        },
-        keys = {
-            {
-                '<leader>hu',
-                function()
-                    require('harpoon.ui').toggle_quick_menu()
-                end,
-                desc = 'Harpoon Quick Menu',
-            },
-            {
-                '<leader>hm',
-                function()
-                    require('harpoon.mark').add_file()
-                end,
-                desc = 'Harpoon File 1',
-            },
-            {
-                '<leader>1',
-                function()
-                    require('harpoon.ui').nav_file(1)
-                end,
-                desc = 'Harpoon File 1',
-            },
-            {
-                '<leader>2',
-                function()
-                    require('harpoon.ui').nav_file(2)
-                end,
-                desc = 'Harpoon File 2',
-            },
-            {
-                '<leader>3',
-                function()
-                    require('harpoon.ui').nav_file(3)
-                end,
-                desc = 'Harpoon File 3',
-            },
-            {
-                '<leader>4',
-                function()
-                    require('harpoon.ui').nav_file(4)
-                end,
-                desc = 'Harpoon File 4',
-            },
-        },
-        config = function()
-            require('harpoon').setup {
-                global_settings = {
-                    -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-                    save_on_toggle = false,
-
-                    -- saves the harpoon file upon every change. disabling is unrecommended.
-                    save_on_change = true,
-
-                    -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-                    enter_on_sendcmd = false,
-
-                    -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-                    tmux_autoclose_windows = false,
-
-                    -- filetypes that you want to prevent from adding to the harpoon list menu.
-                    excluded_filetypes = { 'harpoon' },
-
-                    -- set marks specific to each git branch inside git repository
-                    mark_branch = false,
-                },
-            }
-        end,
+    "ThePrimeagen/harpoon",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
-}
+    config = function()
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set(
+        "n",
+        "<leader>hm",
+        "<cmd>lua require('harpoon.mark').add_file()<cr>",
+        { desc = "Mark file with harpoon" }
+      )
+      keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
+      keymap.set(
+        "n",
+        "<leader>hp",
+        "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
+        { desc = "Go to previous harpoon mark" }
+      )
+    end,
+  }
