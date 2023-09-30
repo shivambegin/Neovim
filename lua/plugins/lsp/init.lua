a75eff1851c6567df73fa237f0ff757683bd3323
Shivam Rajput <shivamrajputcse@gmail.com>
2023-10-01 00:28:35 +0530

    null ls in archived now to using conform and lint

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,10 +3,15 @@ local lspconfig = require 'plugins.lsp.lspconfig'
 local null_ls = require 'plugins.lsp.null-ls'
 local auto_save = require 'plugins.lsp.auto_save'
 local noice = require 'plugins.lsp.noice'
+local conform_nvim = require 'plugins.lsp.conform_nvim'
+local nvim_lint = require 'plugins.lsp.nvim_lint'
+
 return {
     mason,
     lspconfig,
     null_ls,
     auto_save,
-    noice
+    noice,
+    conform_nvim,
+    nvim_lint
 }
