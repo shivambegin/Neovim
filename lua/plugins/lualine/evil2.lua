88e8d3de8ba35319dcd846749bb752b77ce53a05
Shivam Rajput <shivamrajputcse@gmail.com>
2023-10-06 04:01:38 +0530

    color

--- lua/plugins/lualine/evil2.lua
+++ lua/plugins/lualine/evil2.lua
@@ -341,7 +341,7 @@ ins_right({
       return msg
     end,
     icon = ' LSP:',
-    color = { fg = colors.ultraviolet, gui = 'bold' },
+    color = { fg = colors.red, gui = 'bold' },
   })
 
 ins_right({
