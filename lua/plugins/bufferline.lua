4aa937badf2f74151af2cfe0a59ace1c246f4d73
Shivam Rajput <shivamrajputcse@gmail.com>
2023-10-10 02:17:39 +0530

    bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,21 +1,12 @@
-return{
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-    },
-    opts = {
-      options = {
-        numbers = "none",
-        close_command = "bdelete! %d",
-        right_mouse_command = nil,
-        left_mouse_command = "buffer %d",
-        middle_mouse_command = nil,
-        indicator = {
-          icon = "/ ", -- this should be omitted if indicator style is not 'icon'
-          style = "sloped",
-        },
+return {
+	"akinsho/bufferline.nvim",
+	config = function()
+		require("bufferline").setup {
+			-- highlights = require("catppuccin.groups.integrations.bufferline").get(),
+			options = {
+				close_command = "bp|sp|bn|bd! %d",
+				right_mouse_command = "bp|sp|bn|bd! %d",
+				left_mouse_command = "buffer %d",
         buffer_close_icon = "",
         modified_icon = "●",
         close_icon = "",
@@ -24,31 +15,67 @@ return{
         max_name_length = 18,
         max_prefix_length = 15,
         tab_size = 10,
-        diagnostics = true,
-        custom_filter = function(bufnr)
-          -- if the result is false, this buffer will be shown, otherwise, this
-          -- buffer will be hidden.
+				show_tab_indicators = true,
+				indicator = {
+					style = "underline",
+				},
+				enforce_regular_tabs = false,
+				view = "multiwindow",
+				show_buffer_close_icons = true,
+				separator_style = "thin",
+				-- separator_style = "slant",
+				always_show_bufferline = true,
+				diagnostics = true,
+				themable = true,
+			},
+		}
 
-          -- filter out filetypes you don't want to see
-          local exclude_ft = { "qf", "fugitive", "git" }
-          local cur_ft = vim.bo[bufnr].filetype
-          local should_filter = vim.tbl_contains(exclude_ft, cur_ft)
+		-- Buffers belong to tabs
+		local cache = {}
+		local last_tab = 0
 
-          if should_filter then
-            return false
-          end
+		local utils = {}
 
-          return true
-        end,
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        separator_style = "sloped",
-        enforce_regular_tabs = false,
-        always_show_bufferline = true,
-        sort_by = "id",
-      },
-    },
-  }
+		utils.is_valid = function(buf_num)
+			if not buf_num or buf_num < 1 then return false end
+			local exists = vim.api.nvim_buf_is_valid(buf_num)
+			return vim.bo[buf_num].buflisted and exists
+		end
+
+		utils.get_valid_buffers = function()
+			local buf_nums = vim.api.nvim_list_bufs()
+			local ids = {}
+			for _, buf in ipairs(buf_nums) do
+				if utils.is_valid(buf) then ids[#ids + 1] = buf end
+			end
+			return ids
+		end
+
+		local autocmd = vim.api.nvim_create_autocmd
+
+		autocmd("TabEnter", {
+			callback = function()
+				local tab = vim.api.nvim_get_current_tabpage()
+				local buf_nums = cache[tab]
+				if buf_nums then
+					for _, k in pairs(buf_nums) do
+						vim.api.nvim_buf_set_option(k, "buflisted", true)
+					end
+				end
+			end,
+		})
+		autocmd("TabLeave", {
+			callback = function()
+				local tab = vim.api.nvim_get_current_tabpage()
+				local buf_nums = utils.get_valid_buffers()
+				cache[tab] = buf_nums
+				for _, k in pairs(buf_nums) do
+					vim.api.nvim_buf_set_option(k, "buflisted", false)
+				end
+				last_tab = tab
+			end,
+		})
+		autocmd("TabClosed", { callback = function() cache[last_tab] = nil end })
+		autocmd("TabNewEntered", { callback = function() vim.api.nvim_buf_set_option(0, "buflisted", true) end })
+	end,
+}
