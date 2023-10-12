353476c0dd888cae9b7f88781681786cb17c7298
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-13 02:45:00 +0530

    comfig changes blankline, add plugin iluminate

--- lua/plugins/completion/blankline.lua
+++ lua/plugins/completion/blankline.lua
@@ -1,27 +1,68 @@
-return{
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    indent = { char = "▏" },
-    scope = { show_start = false, show_end = false },
-    exclude = {
-      buftypes = {
-        "nofile",
-        "terminal",
-      },
-      filetypes = {
-        "help",
-        "startify",
-        "aerial",
-        "alpha",
-        "dashboard",
-        "lazy",
-        "neogitstatus",
-        "NvimTree",
-        "neo-tree",
-        "Trouble",
-      },
-    },
-  }
+return {
+  {
+      'HiPhish/rainbow-delimiters.nvim', -- colorize parentheses
+      config = function()
+          local rainbow_delimiters = require 'rainbow-delimiters'
+
+          vim.g.rainbow_delimiters = {
+              strategy = {
+                  [''] = rainbow_delimiters.strategy['global'],
+                  vim = rainbow_delimiters.strategy['local'],
+              },
+              query = {
+                  [''] = 'rainbow-delimiters',
+                  lua = 'rainbow-blocks',
+              },
+              highlight = {
+                  'RainbowDelimiterRed',
+                  'RainbowDelimiterYellow',
+                  'RainbowDelimiterBlue',
+                  'RainbowDelimiterOrange',
+                  'RainbowDelimiterGreen',
+                  'RainbowDelimiterViolet',
+                  'RainbowDelimiterCyan',
+              },
+          }
+      end,
+  },
+  {
+      'lukas-reineke/indent-blankline.nvim', -- show indent line
+      dependencies = 'HiPhish/rainbow-delimiters.nvim', -- Use color from rainbow_delimiters
+      main = 'ibl',
+      config = function()
+          local highlight = {
+              'RainbowDelimiterRed',
+              'RainbowDelimiterYellow',
+              'RainbowDelimiterBlue',
+              'RainbowDelimiterOrange',
+              'RainbowDelimiterGreen',
+              'RainbowDelimiterViolet',
+              'RainbowDelimiterCyan',
+          }
+
+          -- vim.g.rainbow_delimiters = { highlight = highlight }
+          require('ibl').setup({
+              -- indent = { char = '│', },
+              exclude = {
+                  filetypes = {
+                      'help', 'startify', 'make', 'NvimTree', 'neo-tree', 'neo-tree-popup',
+                      'dashboard', 'lsp-installer', 'alpha', 'packer', 'Outline', 'mason',
+                      'dap-float', 'dap-repl', 'dapui_scopes', 'dapui_breakpoints',
+                      'dapui_stacks', 'dapui_watches', 'dapui_console', 'norg',
+                      'lazy', 'lspinfo', 'null-ls-info', 'noice',
+                  },
+                  buftypes = {
+                      'terminal', 'nofile', 'quickfix', 'prompt',
+                  },
+              },
+              scope = {
+                  -- enabled = false,
+                  highlight = highlight,
+              },
+          })
+
+          local hooks = require('ibl.hooks')
+          hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+      end,
+  },
 }
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,17 +1,20 @@
 local mason = require 'plugins.lsp.mason'
 local lspconfig = require 'plugins.lsp.lspconfig'
 local auto_save = require 'plugins.lsp.auto_save'
-local noice = require 'plugins.lsp.noice'
+local noice_notify = require 'plugins.lsp.noice_notify'
 local nonels = require 'plugins.lsp.none-ls'
 local lspsaga = require 'plugins.lsp.lspsaga'
 local linting = require 'plugins.lsp.linting'
+local illuminate = require 'plugins.lsp.iluminate'
 
 
 return {
     mason,
     lspconfig,
     auto_save,
-    noice,
+    noice_notify,
     nonels,
     lspsaga,
-    linting,}
+    linting,
+    illuminate,
+}
