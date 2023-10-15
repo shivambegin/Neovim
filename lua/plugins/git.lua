1730f90302cc3da0c50888ea620ed2aa59722eb7
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-16 05:24:17 +0530

    merge : lazy git in git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -76,4 +76,14 @@ return {
             { '<leader>fg', '<cmd>Git<cr>', desc = 'Git status' },
         },
     },
+    {
+      "kdheepak/lazygit.nvim",
+      cmd = { "LazyGit", "LazyGitConfig" },
+      keys = {
+          { "<leader>gg", ":LazyGit<cr>", silent = true, desc = "LazyGit", mode = "n" },
+      },
+      config = function()
+          vim.g.lazygit_floating_window_border_chars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' } -- no borders
+      end,
+  }
 }
