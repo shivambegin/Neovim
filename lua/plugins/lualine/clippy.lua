eaf6a8a92bd00098a42bf2332c7c55724e33be9d
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-16 03:16:45 +0530

    lsp in crispy

--- lua/plugins/lualine/clippy.lua
+++ lua/plugins/lualine/clippy.lua
@@ -70,15 +70,25 @@ return {
                         "filename",
                         symbols = { modified = "  ", readonly = "", unnamed = "" },
                     },
-                    -- {
-                    --     function()
-                    --         return require("nvim-navic").get_location()
-                    --     end,
-                    --     cond = function()
-                    --         return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-                    --     end,
-                    --     color = { fg = colours.nonecolour, bg = colours.bg },
-                    -- },
+                    {
+                        function()
+                            local msg = 'Disabled'
+                            local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                            local clients = vim.lsp.get_active_clients()
+                            if next(clients) == nil then
+                              return msg
+                            end
+                            for _, client in ipairs(clients) do
+                              local filetypes = client.config.filetypes
+                              if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                                return client.name
+                              end
+                            end
+                            return msg
+                          end,
+                          icon = ' LSP:',
+                          color = { fg = colours.magenta, gui = 'bold' },
+                    }
                 },
                 lualine_x = {
                     {
