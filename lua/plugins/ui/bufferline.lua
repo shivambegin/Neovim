d409be1a3becff0298f0007e983d708657c2d2bd
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-21 03:48:03 +0530

    change: bufferline

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,81 +1,26 @@
-return {
+return	{
 	"akinsho/bufferline.nvim",
 	config = function()
-		require("bufferline").setup {
-			-- highlights = require("catppuccin.groups.integrations.bufferline").get(),
+		local ok, bufferline = pcall(require, "bufferline")
+		if not ok then
+			return
+		end
+
+		bufferline.setup({
 			options = {
-				close_command = "bp|sp|bn|bd! %d",
-				right_mouse_command = "bp|sp|bn|bd! %d",
-				left_mouse_command = "buffer %d",
-        buffer_close_icon = "",
-        modified_icon = "●",
-        close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
-        max_name_length = 14,
-        max_prefix_length = 13,
-        tab_size = 10,
-				show_tab_indicators = false,
-				indicator = {
-					style = "underline",
+				offsets = {
+					{ filetype = "NvimTree", text = "", padding = 1 },
+					{ filetype = "neo-tree", text = "File Tree", padding = 1 },
+					{ filetype = "Outline", text = "", padding = 1 },
 				},
-				enforce_regular_tabs = false,
-				view = "multiwindow",
-				show_buffer_close_icons = true,
-				separator_style = "thin",
-				-- separator_style = "slant",
-				always_show_bufferline = true,
-				diagnostics = false,
-				themable = true,
+				max_name_length = 14,
+				max_prefix_length = 13,
+				tab_size = 20,
+				separator_style = { " » ", " » " },
+				style_preset = bufferline.style_preset.no_italic,
 			},
-		}
-
-		-- Buffers belong to tabs
-		local cache = {}
-		local last_tab = 0
-
-		local utils = {}
-
-		utils.is_valid = function(buf_num)
-			if not buf_num or buf_num < 1 then return false end
-			local exists = vim.api.nvim_buf_is_valid(buf_num)
-			return vim.bo[buf_num].buflisted and exists
-		end
-
-		utils.get_valid_buffers = function()
-			local buf_nums = vim.api.nvim_list_bufs()
-			local ids = {}
-			for _, buf in ipairs(buf_nums) do
-				if utils.is_valid(buf) then ids[#ids + 1] = buf end
-			end
-			return ids
-		end
-
-		local autocmd = vim.api.nvim_create_autocmd
-
-		autocmd("TabEnter", {
-			callback = function()
-				local tab = vim.api.nvim_get_current_tabpage()
-				local buf_nums = cache[tab]
-				if buf_nums then
-					for _, k in pairs(buf_nums) do
-						vim.api.nvim_buf_set_option(k, "buflisted", true)
-					end
-				end
-			end,
 		})
-		autocmd("TabLeave", {
-			callback = function()
-				local tab = vim.api.nvim_get_current_tabpage()
-				local buf_nums = utils.get_valid_buffers()
-				cache[tab] = buf_nums
-				for _, k in pairs(buf_nums) do
-					vim.api.nvim_buf_set_option(k, "buflisted", false)
-				end
-				last_tab = tab
-			end,
-		})
-		autocmd("TabClosed", { callback = function() cache[last_tab] = nil end })
-		autocmd("TabNewEntered", { callback = function() vim.api.nvim_buf_set_option(0, "buflisted", true) end })
 	end,
+	event = "VeryLazy",
+	enabled = true,
 }
