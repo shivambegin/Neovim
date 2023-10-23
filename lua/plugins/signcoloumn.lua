7dde5241609af3109706854dea2aba0d830fc7a1
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-24 01:33:18 +0530

    signcol

--- lua/plugins/signcoloumn.lua
+++ lua/plugins/signcoloumn.lua
@@ -1,15 +1,19 @@
-return  {
-    "luukvbaal/statuscol.nvim",
-    config = function()
-      local builtin = require("statuscol.builtin")
-      require("statuscol").setup({
-        ft_ignore = { "neo-tree" },
-        segments = {
-          { sign = { name = { "Diagnostic" } } },
-          { sign = { name = { "Dap.*" } } },
-          { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-          { sign = { name = { "GitSigns.*" } } },
+return {
+  "luukvbaal/statuscol.nvim",
+  event = "BufReadPre",
+  config = function()
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      setopt = true,
+      separator = "",
+      foldfunc = "builtin",
+      segments = {
+        { text = { "%s" }, click = "v:lua.ScSa" },
+        {
+          text = { builtin.lnumfunc, " " },
+          click = "v:lua.ScLa",
         },
-      })
-    end,
-  }
+      },
+    })
+  end,
+}
