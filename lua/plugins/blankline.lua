8dd5e92e509e67383bd4eb64c7005c4bd3da42df
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-24 02:20:59 +0530

    guess-indent

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,68 +1,48 @@
-return {
-  {
-      'HiPhish/rainbow-delimiters.nvim', -- colorize parentheses
-      config = function()
-          local rainbow_delimiters = require 'rainbow-delimiters'
+local M = {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPre",
+    main = "ibl",
+  }
 
-          vim.g.rainbow_delimiters = {
-              strategy = {
-                  [''] = rainbow_delimiters.strategy['global'],
-                  vim = rainbow_delimiters.strategy['local'],
-              },
-              query = {
-                  [''] = 'rainbow-delimiters',
-                  lua = 'rainbow-blocks',
-              },
-              highlight = {
-                  'RainbowDelimiterRed',
-                  'RainbowDelimiterYellow',
-                  'RainbowDelimiterBlue',
-                  'RainbowDelimiterOrange',
-                  'RainbowDelimiterGreen',
-                  'RainbowDelimiterViolet',
-                  'RainbowDelimiterCyan',
-              },
-          }
-      end,
-  },
-  {
-      'lukas-reineke/indent-blankline.nvim', -- show indent line
-      dependencies = 'HiPhish/rainbow-delimiters.nvim', -- Use color from rainbow_delimiters
-      main = 'ibl',
-      config = function()
-          local highlight = {
-              'RainbowDelimiterRed',
-              'RainbowDelimiterYellow',
-              'RainbowDelimiterBlue',
-              'RainbowDelimiterOrange',
-              'RainbowDelimiterGreen',
-              'RainbowDelimiterViolet',
-              'RainbowDelimiterCyan',
-          }
+  M.config = function()
+    local status_ok, indent_blankline = pcall(require, "ibl")
+    if not status_ok then
+      return
+    end
 
-          -- vim.g.rainbow_delimiters = { highlight = highlight }
-          require('ibl').setup({
-              -- indent = { char = '│', },
-              exclude = {
-                  filetypes = {
-                      'help', 'startify', 'make', 'NvimTree', 'neo-tree', 'neo-tree-popup',
-                      'dashboard', 'lsp-installer', 'alpha', 'packer', 'Outline', 'mason',
-                      'dap-float', 'dap-repl', 'dapui_scopes', 'dapui_breakpoints',
-                      'dapui_stacks', 'dapui_watches', 'dapui_console', 'norg',
-                      'lazy', 'lspinfo', 'null-ls-info', 'noice',
-                  },
-                  buftypes = {
-                      'terminal', 'nofile', 'quickfix', 'prompt',
-                  },
-              },
-              scope = {
-                  -- enabled = false,
-                  highlight = highlight,
-              },
-          })
+    indent_blankline.setup({
+      enabled = true,
+      exclude = {
+        filetypes = {
+          "terminal",
+          "nofile",
+          "help",
+          "startify",
+          "dashboard",
+          "packer",
+          "neogitstatus",
+          "NvimTree",
+          "Trouble",
+          "alpha",
+          "packer",
+          "man",
+          "sagasignature",
+          "sagahover",
+          "lspsagafinder",
+          "LspSagaCodeAction",
+          "TelescopePrompt",
+          "NvimTree",
+          "Trouble",
+          "DiffviewFiles",
+          "DiffviewFileHistory",
+          "Outline",
+          "lspinfo",
+          "fugitive",
+          "norg",
+        },
+      },
+      indent = { char = "▏" },
+    })
+  end
 
-          local hooks = require('ibl.hooks')
-          hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-      end,
-  },
-}
+  return M
