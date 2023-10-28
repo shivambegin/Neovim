9454d8ab1c42fa5444b676e8d421d57902509c96
Shivam Rajput <shivamrajput769777@gmail.com>
2023-10-29 03:52:38 +0530

    nvim tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,78 +1,3 @@
--- return {
---     "nvim-tree/nvim-tree.lua",
---     version = "*",
---     lazy = false,
---     dependencies = {
---       "nvim-tree/nvim-web-devicons",
---       "nvim-lua/plenary.nvim",
---       "MunifTanjim/nui.nvim",
---     },
---     keys = {
---       {
---         '<leader>nt',
---         '<cmd>NvimTreeToggle<CR>',
---         desc = 'NvimTreeToggle',
---         },
---       },
---     config = function()
---       require("nvim-tree").setup({
---         sort_by = "case_sensitive",
---         update_focused_file = {
---           enable = true,
---           update_cwd = true,
---         },
---         renderer = {
---           root_folder_modifier = ":t",
---        -- These icons are visible when you install web-devicons
---           icons = {
---             glyphs = {
---               default = "",
---               symlink = "",
---               folder = {
---                 arrow_open = "",
---                 arrow_closed = "",
---                 default = "",
---                 open = "",
---                 empty = "",
---                 empty_open = "",
---                 symlink = "",
---                 symlink_open = "",
---               },
---               git = {
---                 unstaged = "",
---                 staged = "S",
---                 unmerged = "",
---                 renamed = "➜",
---                 untracked = "U",
---                 deleted = "",
---                 ignored = "◌",
---               },
---             },
---           },
---         },
---         diagnostics = {
---           enable = true,
---           show_on_dirs = true,
---           icons = {
---             hint = "",
---             info = "",
---             warning = "",
---             error = "",
---           },
---         },
---         view = {
---           width = 30,
---           side = "right",
---           cursorline = false,
---         },
---         filters = {
---           dotfiles = false,
---           git_ignored = false,
---         },
---       })
---     end,
--- }
-
 local M = {
   "kyazdani42/nvim-tree.lua",
       dependencies = {
@@ -168,7 +93,7 @@ M.config = function()
           git = false,
           folder = true,
           file = true,
-          folder_arrow = false,
+          folder_arrow = true,
         },
       },
     },
@@ -226,7 +151,7 @@ M.config = function()
     view = {
       width = 30,
       side = "right",
-      relativenumber = true,
+      relativenumber = false,
       preserve_window_proportions = true,
       centralize_selection = true,
       cursorline = false,
@@ -239,3 +164,79 @@ M.config = function()
 end
 
 return M
+
+
+-- return {
+--     "nvim-tree/nvim-tree.lua",
+--     version = "*",
+--     lazy = false,
+--     dependencies = {
+--       "nvim-tree/nvim-web-devicons",
+--       "nvim-lua/plenary.nvim",
+--       "MunifTanjim/nui.nvim",
+--     },
+--     keys = {
+--       {
+--         '<leader>nt',
+--         '<cmd>NvimTreeToggle<CR>',
+--         desc = 'NvimTreeToggle',
+--         },
+--       },
+--     config = function()
+--       require("nvim-tree").setup({
+--         sort_by = "case_sensitive",
+--         update_focused_file = {
+--           enable = true,
+--           update_cwd = true,
+--         },
+--         renderer = {
+--           root_folder_modifier = ":t",
+--        -- These icons are visible when you install web-devicons
+--           icons = {
+--             glyphs = {
+--               default = "",
+--               symlink = "",
+--               folder = {
+--                 arrow_open = "",
+--                 arrow_closed = "",
+--                 default = "",
+--                 open = "",
+--                 empty = "",
+--                 empty_open = "",
+--                 symlink = "",
+--                 symlink_open = "",
+--               },
+--               git = {
+--                 unstaged = "",
+--                 staged = "S",
+--                 unmerged = "",
+--                 renamed = "➜",
+--                 untracked = "U",
+--                 deleted = "",
+--                 ignored = "◌",
+--               },
+--             },
+--           },
+--         },
+--         diagnostics = {
+--           enable = true,
+--           show_on_dirs = true,
+--           icons = {
+--             hint = "",
+--             info = "",
+--             warning = "",
+--             error = "",
+--           },
+--         },
+--         view = {
+--           width = 30,
+--           side = "right",
+--           cursorline = false,
+--         },
+--         filters = {
+--           dotfiles = false,
+--           git_ignored = false,
+--         },
+--       })
+--     end,
+-- }
