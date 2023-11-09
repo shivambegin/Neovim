68dcb928bbf91fce7f7da545cc8a8e3b13bc75f1
Shivam Rajput <shivamrajputcse@gmail.com>
2023-11-10 03:26:30 +0530

    #blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,48 +1,34 @@
-local M = {
+return {
+  {
     "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPre",
-    main = "ibl",
-  }
+    config = function()
+      -- insert all the code from: https://github.com/lukas-reineke/indent-blankline.nvim#multiple-indent-colors
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+      local hooks = require("ibl.hooks")
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
 
-  M.config = function()
-    local status_ok, indent_blankline = pcall(require, "ibl")
-    if not status_ok then
-      return
-    end
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      require("ibl").setup({ scope = { highlight = highlight } })
 
-    indent_blankline.setup({
-      enabled = true,
-      exclude = {
-        filetypes = {
-          "terminal",
-          "nofile",
-          "help",
-          "startify",
-          "dashboard",
-          "packer",
-          "neogitstatus",
-          "NvimTree",
-          "Trouble",
-          "alpha",
-          "packer",
-          "man",
-          "sagasignature",
-          "sagahover",
-          "lspsagafinder",
-          "LspSagaCodeAction",
-          "TelescopePrompt",
-          "NvimTree",
-          "Trouble",
-          "DiffviewFiles",
-          "DiffviewFileHistory",
-          "Outline",
-          "lspinfo",
-          "fugitive",
-          "norg",
-        },
-      },
-      indent = { char = "▏" },
-    })
-  end
-
-  return M
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
+  },
+}
