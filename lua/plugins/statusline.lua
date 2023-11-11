873ac279743256f38e6bc2470b3a2aa2c384012d
Shivam Rajput <shivamrajputcse@gmail.com>
2023-11-12 03:02:17 +0530

    statusline lsp

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -5,12 +5,12 @@ return {
     lazy = false,
     enabled = true,
     config = function()
-      require('lualine').setup {
+      require("lualine").setup({
         options = {
           icons_enabled = true,
-          theme = 'onedark',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
+          theme = "onedark",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
           disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -22,31 +22,50 @@ return {
             statusline = 1000,
             tabline = 1000,
             winbar = 1000,
-          }
+          },
         },
         sections = {
-          lualine_a = {'buffers'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
+          lualine_a = { "buffers" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = {
+                        {
+              function()
+                local msg = "Loading...."
+                local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+                local clients = vim.lsp.get_active_clients()
+                if next(clients) == nil then
+                  return msg
+                end
+                for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    return client.name
+                  end
+                end
+                return msg
+              end,
+              icon = "LSP~>",
+              },
+           },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
         },
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
           lualine_y = {},
-          lualine_z = {}
+          lualine_z = {},
         },
         tabline = {},
         winbar = {},
         inactive_winbar = {},
-        extensions = {}
-      }
+        extensions = {},
+      })
     end,
-  }
+  },
 }
 -- return {
 --   {
