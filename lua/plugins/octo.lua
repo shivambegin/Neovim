3de1e7da6f3eae33fddc883f6e367645f18e721d
Shivam Rajput <shivamrajput769777@gmail.com>
2023-11-14 22:34:59 +0530

    fix

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,16 +1,17 @@
 return {
-    'pwntester/octo.nvim',
-    cmd = 'Octo',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope.nvim',
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        require('octo').setup { enable_builtin = true }
-        vim.cmd [[hi OctoEditable guibg=none]]
-    end,
-    keys = {
-        { '<leader>o', '<cmd>Octo<cr>', desc = 'Octo' },
-    },
+  "pwntester/octo.nvim",
+  cmd = "Octo",
+  enabled = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("octo").setup({ enable_builtin = true })
+    vim.cmd([[hi OctoEditable guibg=none]])
+  end,
+  keys = {
+    { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
+  },
 }
--- lua/plugins/winbar.lua
+++ lua/plugins/winbar.lua
@@ -3,7 +3,7 @@ return {
     "SmiteshP/nvim-navic",
     enabled = true,
     event = "BufReadPre",
-    dependencies = { "folke/tokyonight.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
     config = function()
       vim.g.navic_silence = true
       require("nvim-navic").setup({ separator = " › ", highlight = true, depth_limit = 3 })
