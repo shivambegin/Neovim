f625ffda991b5ced21685bb588bb2e01a4937be9
Shivam Rajput <shivamrajputcse@gmail.com>
2023-11-17 03:33:56 +0530

    mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -73,4 +73,22 @@ return {
     "xorid/asciitree.nvim",
     cmd = "AsciiTree",
   },
+  {
+		"echasnovski/mini.hipatterns",
+		event = "BufReadPre",
+		opts = {
+			highlighters = {
+				hsl_color = {
+					pattern = "hsl%(%d+,? %d+,? %d+%)",
+					group = function(_, match)
+						local utils = require("craftzdog.utils")
+						local h, s, l = match:match("hsl%((%d+),? (%d+),? (%d+)%)")
+						h, s, l = tonumber(h), tonumber(s), tonumber(l)
+						local hex_color = utils.hslToHex(h, s, l)
+						return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+					end,
+				},
+			},
+		},
+	},
 }
