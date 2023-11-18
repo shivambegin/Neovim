e2e1f73190e460e7958b61276c9e1f8b2b4819ec
Shivam Rajput <shivamrajput769777@gmail.com>
2023-11-19 05:18:41 +0530

    refactor

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,19 +1,4 @@
 return {
-  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
-  {
-    "danymat/neogen",
-    keys = {
-      {
-        "<leader>cc",
-        function()
-          require("neogen").generate({})
-        end,
-        desc = "Neogen Comment",
-      },
-    },
-    opts = { snippet_engine = "luasnip" },
-  },
-
   -- Incremental rename
   {
     "smjonas/inc-rename.nvim",
@@ -55,35 +40,4 @@ return {
     end,
   },
 
-  -- Better increase/descrease
-  {
-    "monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-    },
-    config = function()
-      local augend = require("dial.augend")
-      require("dial.config").augends:register_group({
-        default = {
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.date.alias["%Y/%m/%d"],
-          augend.constant.alias.bool,
-          augend.semver.alias.semver,
-          augend.constant.new({ elements = { "let", "const" } }),
-        },
-      })
-    end,
-  },
-
-  {
-    "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    cmd = "SymbolsOutline",
-    opts = {
-      position = "right",
-    },
-  },
 }
