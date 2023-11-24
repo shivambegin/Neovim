ebd4ae155c92d9edb3f82091b581d9b87cd26ad2
Shivam Rajput <shivamrajputcse@gmail.com>
2023-11-24 14:08:15 +0530

    config

--- lua/shivam/plugins/filebrowser/nvim-tree.lua
+++ lua/shivam/plugins/filebrowser/nvim-tree.lua
@@ -1,10 +1,10 @@
 local M = {
   "kyazdani42/nvim-tree.lua",
-      dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
   cmd = "NvimTree",
   keys = {
     {
@@ -150,7 +150,7 @@ M.config = function()
     },
     view = {
       width = 30,
-      side = "right",
+      side = "left",
       relativenumber = false,
       preserve_window_proportions = true,
       centralize_selection = true,
@@ -164,79 +164,3 @@ M.config = function()
 end
 
 return M
-
-
--- return {
---     "nvim-tree/nvim-tree.lua",
---     version = "*",
---     lazy = false,
---     dependencies = {
---       "nvim-tree/nvim-web-devicons",
---       "nvim-lua/plenary.nvim",
---       "MunifTanjim/nui.nvim",
---     },
---     keys = {
---       {
---         '<leader>nt',
---         '<cmd>NvimTreeToggle<CR>',
---         desc = 'NvimTreeToggle',
---         },
---       },
---     config = function()
---       require("nvim-tree").setup({
---         sort_by = "case_sensitive",
---         update_focused_file = {
---           enable = true,
---           update_cwd = true,
---         },
---         renderer = {
---           root_folder_modifier = ":t",
---        -- These icons are visible when you install web-devicons
---           icons = {
---             glyphs = {
---               default = "",
---               symlink = "",
---               folder = {
---                 arrow_open = "",
---                 arrow_closed = "",
---                 default = "",
---                 open = "",
---                 empty = "",
---                 empty_open = "",
---                 symlink = "",
---                 symlink_open = "",
---               },
---               git = {
---                 unstaged = "",
---                 staged = "S",
---                 unmerged = "",
---                 renamed = "➜",
---                 untracked = "U",
---                 deleted = "",
---                 ignored = "◌",
---               },
---             },
---           },
---         },
---         diagnostics = {
---           enable = true,
---           show_on_dirs = true,
---           icons = {
---             hint = "",
---             info = "",
---             warning = "",
---             error = "",
---           },
---         },
---         view = {
---           width = 30,
---           side = "right",
---           cursorline = false,
---         },
---         filters = {
---           dotfiles = false,
---           git_ignored = false,
---         },
---       })
---     end,
--- }
