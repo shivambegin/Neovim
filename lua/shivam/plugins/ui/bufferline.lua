07bf977d17f2117e06d3dedfacc79d3a2a42db38
shivam rajput <shivamrajputcse@gmail.com>
2023-11-26 13:41:30 +0530

    bufferline enabled

--- lua/shivam/plugins/ui/bufferline.lua
+++ lua/shivam/plugins/ui/bufferline.lua
@@ -1,17 +1,15 @@
 return {
   "akinsho/bufferline.nvim",
-  enabled = false,
+  enabled = true,
   event = "VeryLazy",
-  keys = {
-    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  },
-  opts = {
-    options = {
-      mode = "tabs",
-      --separator_style = "slant",
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-    },
-  },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        mode = "buffers",
+        diagonostics = true,
+        show_close_icon = false,
+        show_buffer_close_icon = false,
+      },
+    })
+  end,
 }
--- lua/shivam/plugins/ui/transparent.lua
+++ lua/shivam/plugins/ui/transparent.lua
@@ -54,6 +54,7 @@ return {
           "NavicText",
           "NavicSeparator",
           "LazyNormal",
+          "BufferlineNormal",
         }, -- table: additional groups that should be cleared
         exclude_groups = {}, -- table: groups you don't want to clear
       })
