2df5fab607917dda5761a7a249591a2b33208769
Shivam Rajput <shivamrajputcse@gmail.com>
2023-12-10 00:17:39 +0530

    config

---
+++ lua/shivam/plugins/filebrowser/search.lua
@@ -0,0 +1,18 @@
+--- lazy nvim
+return {
+  "FabianWirth/search.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    require("search").setup({
+      append_tabs = { -- append_tabs will add the provided tabs to the default ones
+        {
+          name = "Commits",
+          tele_func = require("telescope.builtin").git_commits,
+          available = function()
+            return vim.fn.isdirectory(".git") == 1
+          end,
+        },
+      },
+    })
+  end,
+}
--- lua/shivam/plugins/undotree.lua
+++ lua/shivam/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
-    'mbbill/undotree',
-    keys = {
-        { '<leader>u', '<cmd>UndotreeToggle<cr>', desc = 'Toggle Undotree' },
-    },
+  "mbbill/undotree",
+  keys = {
+    { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
+  },
 }
