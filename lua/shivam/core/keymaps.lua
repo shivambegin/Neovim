39ae98d027b72508135d0770fe77146bcc7bc9cf
shivam <shivam rajput>
2024-01-05 07:06:37 +0530

    lsp

--- lua/shivam/core/keymaps.lua
+++ lua/shivam/core/keymaps.lua
@@ -1,100 +1,102 @@
-local map = vim.keymap.set
-
 -- This is going to get me cancelled(primegen inspired : )
-map("i", "<C-c>", "<Esc>")
-map("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("i", "<C-c>", "<Esc>")
 -- General {{{
 --
-map("n", "<leader>ex", vim.cmd.Ex)
-map("n", "<leader><leader>", function()
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
-map("n", "<leader>fo", vim.lsp.buf.format)
---utility mappings
-map("n", "n", "nzzzv") -- keep the cursor centered when doing 'n'
-map("n", "N", "Nzzzv") -- keep the cursor centered when doing 'N'
-map("n", "J", "mzJ`z", { desc = "Join line without moving the cursor" })
-map("n", "cn", "*``cgn", { desc = "Change next match by pressing dot (.)" })
-map("n", "cN", "*``cgN", { desc = "Change previous match by pressing dot (.)" })
-map("n", "<leader>vp", "`[v`]<CR>", { desc = "Select pasted text" })
-map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
-map(
+vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format)
+--utility vim.keymap.setpings
+vim.keymap.set("n", "n", "nzzzv") -- keep the cursor centered when doing 'n'
+vim.keymap.set("n", "N", "Nzzzv") -- keep the cursor centered when doing 'N'
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line without moving the cursor" })
+vim.keymap.set("n", "cn", "*``cgn", { desc = "Change next match by pressing dot (.)" })
+vim.keymap.set("n", "cN", "*``cgN", { desc = "Change previous match by pressing dot (.)" })
+vim.keymap.set("n", "<leader>vp", "`[v`]<CR>", { desc = "Select pasted text" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
+vim.keymap.set(
   "n",
   "<leader>z",
   ":%s/<C-R><C-W>/<C-R>0/g<CR>",
   { desc = 'Replace word under cursor with register "0" content globally' }
 )
 
-map("n", "<leader>ts", "<cmd>set spell!<CR>", { desc = "Toggle spell check" })
-map("n", "<leader>th", "<cmd>set hlsearch!<CR>", { desc = "Toggle highlights (hlsearch)" })
+vim.keymap.set("n", "<leader>ts", "<cmd>set spell!<CR>", { desc = "Toggle spell check" })
+vim.keymap.set("n", "<leader>th", "<cmd>set hlsearch!<CR>", { desc = "Toggle highlights (hlsearch)" })
 
-map({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
-map({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
 
-map({ "i" }, "<C-l>", "<Right>", { desc = "Move cursor to right" })
-map({ "i" }, "<C-h>", "<Left>", { desc = "Move cursor to right" })
+vim.keymap.set({ "i" }, "<C-l>", "<Right>", { desc = "Move cursor to right" })
+vim.keymap.set({ "i" }, "<C-h>", "<Left>", { desc = "Move cursor to right" })
 
 -- }}}
 
 -- Better window management {{{
 
-map("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-map("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
-map("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
-map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
-map("n", "<C-Up>", "<cmd>resize -2<CR>", { desc = "Resize split up" })
-map("n", "<C-Down>", "<cmd>resize +2<CR>", { desc = "Resize split down" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize split left" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Resize split right" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<CR>", { desc = "Resize split up" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<CR>", { desc = "Resize split down" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize split left" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Resize split right" })
 -- }}}
 
 -- save and quit {{{
 
-map("n", "<leader>Q", "<cmd>wq!<CR>", { desc = "Force save and  quit" })
+vim.keymap.set("n", "<leader>Q", "<cmd>wq!<CR>", { desc = "Force save and  quit" })
 -- }}}
 
 -- Buffers {{{
 
-map("n", "<leader>ba", "<cmd>bufdo bd<CR>", { desc = "close all buffers" })
-map("n", "<leader>bo", "<cmd>w <bar> %bd <bar> e# <bar> bd#<CR>", { desc = "Close all buffers except current one" })
-map("n", "<leader>q", "<cmd>q!<CR>", { desc = "Close buffer/window" })
-map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
-map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
-map("n", "]b", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
-map("n", "[b", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
+vim.keymap.set("n", "<leader>ba", "<cmd>bufdo bd<CR>", { desc = "close all buffers" })
+vim.keymap.set(
+  "n",
+  "<leader>bo",
+  "<cmd>w <bar> %bd <bar> e# <bar> bd#<CR>",
+  { desc = "Close all buffers except current one" }
+)
+vim.keymap.set("n", "<leader>q", "<cmd>q!<CR>", { desc = "Close buffer/window" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
+vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "[b", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
 
 -- }}}
 
 -- Tabs {{{
 
-map("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New tab" })
-map("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close tab" })
-map("n", "]t", ":tabnext<CR>", { desc = "Go to next tab" })
-map("n", "[t", ":tabprevious<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New tab" })
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close tab" })
+vim.keymap.set("n", "]t", ":tabnext<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "[t", ":tabprevious<CR>", { desc = "Go to previous tab" })
 -- }}}
 
 -- Visual {{{
 
 -- Stay in indent mode
-map("v", "<", "<gv", { desc = "(V) Indent to left" })
-map("v", ">", ">gv", { desc = "(V) Indent to right" })
+vim.keymap.set("v", "<", "<gv", { desc = "(V) Indent to left" })
+vim.keymap.set("v", ">", ">gv", { desc = "(V) Indent to right" })
 -- }}}
 
-map("n", "<Leader>lo", "<cmd>lopen<CR>", { desc = "Open location list" })
-map("n", "<Leader>lc", "<cmd>lclose<CR>", { desc = "Close location list" })
-map("n", "<Leader>ln", "<cmd>lnext<CR>", { desc = "Next location list item" })
-map("n", "<Leader>lp", "<cmd>lprev<CR>", { desc = "Previous location list item" })
+vim.keymap.set("n", "<Leader>lo", "<cmd>lopen<CR>", { desc = "Open location list" })
+vim.keymap.set("n", "<Leader>lc", "<cmd>lclose<CR>", { desc = "Close location list" })
+vim.keymap.set("n", "<Leader>ln", "<cmd>lnext<CR>", { desc = "Next location list item" })
+vim.keymap.set("n", "<Leader>lp", "<cmd>lprev<CR>", { desc = "Previous location list item" })
 -- }}}
 
 -- vim:fdm=marker:fdl=0
 
-map("n", "<leader>la", "<cmd>Lazy<CR>", { desc = "Lazy" })
-map("n", "<leader>ma", "<cmd>Mason<CR>", { desc = "Mason" })
-map("n", "<leader>ch", "<cmd>checkhealth<CR>", { desc = "checkhealth" })
-map("n", "<leader>ee", "<cmd>E<CR>", { desc = "netrw" })
--- Visual Maps
-map("v", "<leader>r", '"hy:%s/<C-r>h//g<left><left>') -- Replace all instances of highlighted words
-map("v", "<C-s>", ":sort<CR>") -- Sort highlighted text in visual mode with Control+S
-map("v", "K", ":m '>-2<CR>gv=gv") -- Move current line up
-map("v", "J", ":m '>+1<CR>gv=gv") -- Move current line down
+vim.keymap.set("n", "<leader>la", "<cmd>Lazy<CR>", { desc = "Lazy" })
+vim.keymap.set("n", "<leader>ma", "<cmd>Mason<CR>", { desc = "Mason" })
+vim.keymap.set("n", "<leader>ch", "<cmd>checkhealth<CR>", { desc = "checkhealth" })
+vim.keymap.set("n", "<leader>ee", "<cmd>E<CR>", { desc = "netrw" })
+-- Visual vim.keymap.sets
+vim.keymap.set("v", "<leader>r", '"hy:%s/<C-r>h//g<left><left>') -- Replace all instances of highlighted words
+vim.keymap.set("v", "<C-s>", ":sort<CR>") -- Sort highlighted text in visual mode with Control+S
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv") -- Move current line up
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move current line down
--- lua/shivam/plugins/lsp/none-ls.lua
+++ lua/shivam/plugins/lsp/none-ls.lua
@@ -16,8 +16,8 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        --"black", -- python formatter
-        --"pylint", -- python linter
+        "black", -- python formatter
+        "pylint", -- python linter
         "eslint_d", -- js linter
       },
     })
@@ -41,9 +41,9 @@ return {
           extra_filetypes = { "svelte" },
         }), -- js/ts formatter
         formatting.stylua, -- lua formatter
-        --formatting.isort,
-        --formatting.black,
-        --diagnostics.pylint,
+        formatting.isort,
+        formatting.black,
+        diagnostics.pylint,
         diagnostics.eslint_d.with({ -- js/ts linter
           condition = function(utils)
             return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
