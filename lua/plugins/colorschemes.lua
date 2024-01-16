d9a2a9a9d155967efdf4dbfef581f4136bd4a27f
shivam <shivam rajput>
2024-01-16 19:16:27 +0530

    refactor

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,9 +2,27 @@ return {
   {
     "bluz71/vim-nightfly-guicolors",
     priority = 1000, -- make sure to load this before all the other start plugins
+    enabled = true,
+    lazy = false,
     config = function()
       -- load the colorscheme here
       vim.cmd([[colorscheme nightfly]])
+    end,
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+      })
+      vim.cmd("colorscheme cyberdream") -- set the colorscheme
     end,
   },
 }
