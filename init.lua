6d2f87bc969dae6ef25cd50f8a0cc1a04b94345c
shivam rajput <shivamrajput769777@gmail.com>
2024-01-24 05:06:05 +0530

    refactor

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("config.keymaps")
 require("config.options")
-require("config.abbreviations")
 require("config.lazy")
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -16,13 +16,7 @@ return {
         theme = "auto",
       },
       sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-        },
+        lualine_x = {},
         lualine_a = {
           {
             "buffers",
