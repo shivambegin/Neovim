b5b59aebf2d2e9e68ab51aa6c7c917ad8fc48fa6
shivam rajput <shivamrajput769777@gmail.com>
2024-01-26 19:36:08 +0530

    refactor

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,33 @@
 return {
+  -- Lua
 
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      })
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      vim.cmd("colorscheme poimandres")
+    end,
+  },
+
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("catppuccin").setup({})
+    end,
+  },
   {
     "Shatur/neovim-ayu",
     lazy = false,
@@ -57,7 +85,7 @@ return {
     config = function()
       require("rose-pine").setup({
 
-        vim.cmd("colorscheme rose-pine"),
+        -- vim.cmd("colorscheme rose-pine"),
         -- vim.cmd('colorscheme rose-pine-main')
         -- vim.cmd('colorscheme rose-pine-moon')
         -- vim.cmd("colorscheme rose-pine-dawn"),
