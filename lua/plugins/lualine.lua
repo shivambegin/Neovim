2cfce42d7510b5c592d58f9f62eed7c23b67746c
shivam rajput <shivamrajput769777@gmail.com>
2024-01-26 09:18:33 +0530

    refactor

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
   },
   opts = {
     options = {
-      theme = "papercolor_dark",
+      theme = "auto",
       component_separators = { left = "│", right = "│" },
       section_separators = { left = "", right = "" },
       globalstatus = true,
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -2,5 +2,29 @@ return {
   "mfussenegger/nvim-dap",
   dependencies = {
     "leoluz/nvim-dap-go",
+    "rcarriga/nvim-dap-ui",
   },
+  config = function()
+    require("dap-go").setup()
+    require("dapui").setup()
+    local dap = require("dap")
+    local keymap = vim.keymap
+    local dapui = require("dapui")
+
+    keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+    keymap.set("n", "<leader>dc", dap.continue, {})
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  end,
 }
