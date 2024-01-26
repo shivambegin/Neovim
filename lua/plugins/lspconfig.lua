911b093bbe2c3ca0b385802f335951fb6316d371
shivam rajput <shivamrajput769777@gmail.com>
2024-01-27 04:43:31 +0530

    refactor

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -25,8 +25,8 @@ return {
       opts.desc = "Go to declaration"
       keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      -- opts.desc = "Show LSP definitions"
+      -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
       opts.desc = "Show LSP implementations"
       keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
@@ -34,8 +34,8 @@ return {
       opts.desc = "Show LSP type definitions"
       keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      -- opts.desc = "See available code actions"
+      -- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
       opts.desc = "Smart rename"
       keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
@@ -46,14 +46,14 @@ return {
       opts.desc = "Show line diagnostics"
       keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+      -- opts.desc = "Go to previous diagnostic"
+      -- keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+      --
+      -- opts.desc = "Go to next diagnostic"
+      -- keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+      -- opts.desc = "Show documentation for what is under cursor"
+      -- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
       opts.desc = "Restart LSP"
       keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,17 +1,41 @@
 return {
   "nvimdev/lspsaga.nvim",
   config = function()
+    local keymap = vim.keymap
+
     require("lspsaga").setup({
-      symbol_in_winbar = {
+      symbols_in_winbar = {
         enable = false,
       },
       ui = {
         border = "rounded",
       },
       lightbulb = {
-        enable = true,
+        enable = false,
       },
     })
+
+    keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>")
+    keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>")
+    keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<cr>")
+
+    local builtin = require("telescope.builtin")
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", opts)
+        -- vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", opts)
+        vim.keymap.set("n", "gr", builtin.lsp_references, opts)
+      end,
+    })
+
+    vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", { silent = true })
 
     -- error lens
     vim.fn.sign_define({
