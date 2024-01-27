1bb24e185dd14106cadb72333a3ffa5c66ae76e4
shivam rajput <shivamrajput769777@gmail.com>
2024-01-27 05:34:21 +0530

    keybinds

--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     local opts = { noremap = true, silent = true }
     -- Normal-mode commands
-    vim.keymap.set("n", "<A-Down>", ":MoveLine(1)<CR>", opts)
-    vim.keymap.set("n", "<A-Up>", ":MoveLine(-1)<CR>", opts)
+    vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>", opts)
+    vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>", opts)
 
     -- Visual-mode commands
-    vim.keymap.set("v", "<A-Down>", ":MoveBlock(1)<CR>", opts)
-    vim.keymap.set("v", "<A-Up>", ":MoveBlock(-1)<CR>", opts)
+    vim.keymap.set("v", "<A-j>", ":MoveBlock(1)<CR>", opts)
+    vim.keymap.set("v", "<A-k>", ":MoveBlock(-1)<CR>", opts)
   end,
 }
