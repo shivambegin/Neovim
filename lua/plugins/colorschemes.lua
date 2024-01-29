397263d683c54f45d5fee2bd4c994571bbac9600
shivam rajput <shivamrajput769777@gmail.com>
2024-01-30 03:39:18 +0530

    theme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     config = function(plugin)
       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
+      vim.cmd([[colorscheme aura-soft-dark-soft-text]])
     end,
   },
   {
