58dd8d0b02487e786457a1c7c1bb047239c6682e
shivam rajput <shivamrajput769777@gmail.com>
2024-02-09 13:39:45 +0530

    options

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,25 @@
 return {
   {
+    "sainnhe/gruvbox-material",
+    enabled = true,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "soft" -- soft, medium, hard
+      vim.g.gruvbox_material_ui_contrast = "soft" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "bright" -- Background of floating windows
+      vim.g.gruvbox_material_statusline_style = "material"
+      vim.g.gruvbox_material_cursor = "auto"
+
+      -- vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
+      -- vim.g.gruvbox_material_colors_override = { bg0 = "#121212" }
+      -- vim.g.gruvbox_material_better_performance = 1
+
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
     "baliestri/aura-theme",
     lazy = false,
     priority = 1000,
@@ -103,7 +123,7 @@ return {
       require("rose-pine").setup({
 
         -- vim.cmd("colorscheme rose-pine"),
-        vim.cmd("colorscheme rose-pine-main"),
+        -- vim.cmd('colorscheme rose-pine-main')
         -- vim.cmd('colorscheme rose-pine-moon')
         -- vim.cmd("colorscheme rose-pine-dawn"),
       })
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,44 +2,69 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "meuter/lualine-so-fancy.nvim",
-    "nvim-tree/nvim-web-devicons",
   },
-  lazy = false,
   enabled = true,
-  opts = {
-    options = {
-      theme = "auto",
-      component_separators = { left = "│", right = "│" },
-      section_separators = { left = "", right = "" },
-      globalstatus = true,
-      refresh = {
-        statusline = 100,
+  lazy = false,
+  event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+  config = function()
+    -- local icons = require("config.icons")
+    require("lualine").setup({
+      options = {
+        -- theme = "auto",
+        theme = "catppuccin",
+        globalstatus = true,
+        icons_enabled = true,
+        -- component_separators = { left = "│", right = "│" },
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {
+            "alfa-nvim",
+            "help",
+            "neo-tree",
+            "Trouble",
+            "spectre_panel",
+            "toggleterm",
+          },
+          winbar = {},
+        },
       },
-    },
-    sections = {
-      lualine_a = {
-        -- { "fancy_mode", width = 3 },
-        { "buffers" },
+      sections = {
+        lualine_a = {},
+        lualine_b = {
+          "fancy_branch",
+        },
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- 2 for full path
+            symbols = {
+              modified = "  ",
+              -- readonly = "  ",
+              -- unnamed = "  ",
+            },
+          },
+          { "fancy_diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", info = " " } },
+          { "fancy_searchcount" },
+        },
+        lualine_x = {
+          "fancy_lsp_servers",
+          "fancy_diff",
+          "progress",
+        },
+        lualine_y = {},
+        lualine_z = {},
       },
-      lualine_b = {
-        { "fancy_branch" },
-        { "fancy_diff" },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        -- lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
       },
-      lualine_c = {
-        { "fancy_cwd", substitute_home = true },
-      },
-      lualine_x = {
-        { "fancy_macro" },
-        { "fancy_diagnostics" },
-        { "fancy_searchcount" },
-        { "fancy_location" },
-      },
-      lualine_y = {
-        { "fancy_filetype", ts_icon = "" },
-      },
-      lualine_z = {
-        { "fancy_lsp_servers" },
-      },
-    },
-  },
+      tabline = {},
+      extensions = { "neo-tree", "lazy" },
+    })
+  end,
 }
