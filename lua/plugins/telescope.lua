5a73eaec0bc438d9b4a6f7ffce7f0fcd77bac82c
shivam rajput <shivamrajput769777@gmail.com>
2024-02-09 16:14:14 +0530

    refactor

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,189 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>tt", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>ch", builtin.colorscheme, {})
 return {
   {
     "nvim-telescope/telescope.nvim",
-    version = false, -- telescope did only one release, so use HEAD for now
-    dependencies = {
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "zig" },
-      { "nvim-telescope/telescope-file-browser.nvim" },
-      { "nvim-lua/plenary.nvim" },
-    },
     cmd = "Telescope",
+    version = false,
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "zig" },
+      "nvim-telescope/telescope-ui-select.nvim",
+      "nvim-telescope/telescope-frecency.nvim",
+    },
     config = function()
-      require("telescope").setup({})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      -- This is your opts table
-      require("telescope").setup({
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>tt", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>ch", builtin.colorscheme, {})
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local trouble = require("trouble.providers.telescope")
+      local icons = require("config.icons")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "TelescopeResults",
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+
+      local function formattedName(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then
+          return tail
+        end
+        return string.format("%s\t\t%s", tail, parent)
+      end
+
+      telescope.setup({
+        file_ignore_patterns = { "%.git/." },
+        -- borderchars = { "█", " ", "▀", "█", "█", " ", " ", "▀" },
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+              ["<C-t>"] = trouble.open_with_trouble,
+            },
+
+            n = { ["<C-t>"] = trouble.open_with_trouble },
+          },
+          previewer = false,
+          prompt_prefix = " " .. icons.ui.Telescope .. " ",
+          selection_caret = icons.ui.BoldArrowRight .. " ",
+          file_ignore_patterns = { "node_modules", "package-lock.json" },
+          initial_mode = "insert",
+          select_strategy = "reset",
+          sorting_strategy = "ascending",
+          color_devicons = true,
+          set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+          layout_config = {
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob=!.git/",
+          },
+        },
+        pickers = {
+          find_files = {
+            previewer = false,
+            path_display = formattedName,
+            layout_config = {
+              height = 0.4,
+              prompt_position = "top",
+              preview_cutoff = 120,
+            },
+          },
+          git_files = {
+            previewer = false,
+            path_display = formattedName,
+            layout_config = {
+              height = 0.4,
+              prompt_position = "top",
+              preview_cutoff = 120,
+            },
+          },
+          buffers = {
+            path_display = formattedName,
+            mappings = {
+              i = {
+                ["<c-d>"] = actions.delete_buffer,
+              },
+              n = {
+                ["<c-d>"] = actions.delete_buffer,
+              },
+            },
+            previewer = false,
+            initial_mode = "normal",
+            -- theme = "dropdown",
+            layout_config = {
+              height = 0.4,
+              width = 0.6,
+              prompt_position = "top",
+              preview_cutoff = 120,
+            },
+          },
+          current_buffer_fuzzy_find = {
+            previewer = true,
+            layout_config = {
+              prompt_position = "top",
+              preview_cutoff = 120,
+            },
+          },
+          live_grep = {
+            only_sort_text = true,
+            previewer = true,
+          },
+          grep_string = {
+            only_sort_text = true,
+            previewer = true,
+          },
+          lsp_references = {
+            show_line = false,
+            previewer = true,
+          },
+          treesitter = {
+            show_line = false,
+            previewer = true,
+          },
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
         extensions = {
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({
-              -- even more opts
+              previewer = false,
+              initial_mode = "normal",
+              sorting_strategy = "ascending",
+              layout_strategy = "horizontal",
+              layout_config = {
+                horizontal = {
+                  width = 0.5,
+                  height = 0.4,
+                  preview_width = 0.6,
+                },
+              },
             }),
+          },
+          frecency = {
+            default_workspace = "CWD",
+            show_scores = true,
+            show_unindexed = true,
+            disable_devicons = false,
+            ignore_patterns = {
+              "*.git/*",
+              "*/tmp/*",
+              "*/lua-language-server/*",
+            },
           },
         },
       })
-      require("telescope").load_extension("ui-select")
+      telescope.load_extension("ui-select")
+      telescope.load_extension("refactoring")
+      telescope.load_extension("frecency")
+      telescope.load_extension("notify")
     end,
   },
 }
