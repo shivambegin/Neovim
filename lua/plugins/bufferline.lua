70beaad932ad80c746f059ce36f81d3f5243ffb7
shivam rajput <shivamrajput769777@gmail.com>
2024-02-11 15:35:27 +0530

    refactor

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "buffers",
+      -- separator_style = "slope",
+    },
+  },
+}
