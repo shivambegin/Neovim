b700e1251c4a730f11f15df29f1300c69c8812d8
shivam rajput <shivamrajput769777@gmail.com>
2024-02-15 19:05:56 +0530

    Change: theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -84,8 +84,8 @@ return {
     end
 
     local lsp_progess = function()
-      msg = msg or "LS Inactive"
-      local buf_clients = vim.lsp.buf_get_clients()
+      msg = msg or "lsp not attached"
+      local buf_clients = vim.lsp.get_clients()
       if next(buf_clients) == nil then
         -- TODO: clean up this if statement
         if type(msg) == "boolean" or #msg == 0 then
