6047a0d5be354028efb453d7e4c34358bb4a2f95
shivam rajput <shivamrajput769777@gmail.com>
2024-02-16 16:44:28 +0530

    refactor

---
+++ lua/plugins/smooth-cursor.lua
@@ -0,0 +1,13 @@
+return {
+  "gen740/SmoothCursor.nvim",
+  enabled = true,
+  config = function()
+    require("smoothcursor").setup({
+      cursor = "",
+      fancy = {
+        enable = true,
+        head = { cursor = "" },
+      },
+    })
+  end,
+}
