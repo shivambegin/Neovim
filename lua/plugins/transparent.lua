11255612c44b77ea0736313cceb238da949b64f6
shivam rajput <shivamrajput769777@gmail.com>
2024-02-19 20:00:21 +0530

    refactor

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -15,7 +15,7 @@ return {
       require("transparent").clear_prefix("Notify")
       require("transparent").clear_prefix("GitSigns")
       require("transparent").clear_prefix("Satellite")
-      -- require("transparent").clear_prefix("lualine")
+      require("transparent").clear_prefix("lualine")
       require("transparent").clear_prefix("BufferLine")
       require("transparent").clear_prefix("barbecue")
       require("transparent").clear_prefix("Mini")
