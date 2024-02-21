c30bd352d8d52e8d6b6910d1bee08353b3d11dcc
shivam rajput <shivamrajput769777@gmail.com>
2024-02-22 02:16:43 +0530

    refactor

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,11 +2,7 @@ return {
   "echasnovski/mini.nvim",
   version = false,
   config = function()
-    -- require("mini.statusline").setup({})
     require("mini.surround").setup({})
-    -- require("mini.indentscope").setup({
-    --   symbol = "│",
-    -- })
     require("mini.pairs").setup({})
     require("mini.move").setup({})
     require("mini.bracketed").setup({
