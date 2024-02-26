31db9032bcd24d08450f8b130bd0f4a0ee5b538c
shivam rajput <shivamrajput769777@gmail.com>
2024-02-27 02:01:28 +0530

    refactor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,20 @@
 return {
   {
-    "sainnhe/sonokai",
+    "rose-pine/neovim",
+    name = "rose-pine",
     priority = 1000,
     config = function()
-      vim.g.sonokai_transparent_background = "1"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+  {
+    "sainnhe/sonokai",
+    -- priority = 1000,
+    config = function()
+      -- vim.g.sonokai_transparent_background = "1"
+      -- vim.g.sonokai_enable_italic = "1"
+      -- vim.g.sonokai_style = "andromeda"
+      -- vim.cmd.colorscheme("sonokai")
     end,
   },
 }
