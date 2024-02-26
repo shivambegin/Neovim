ed78881111e3a607aebcf6abac92768b0a7e85bc
shivam rajput <shivamrajput769777@gmail.com>
2024-02-26 23:00:44 +0530

    refactor

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,6 @@
 return {
   "akinsho/bufferline.nvim",
+  enabled = false,
   event = "VeryLazy",
   keys = {
     { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,24 +1,29 @@
 return {
-    "ThePrimeagen/harpoon",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      -- set keymaps
-      local keymap = vim.keymap -- for conciseness
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { { "nvim-lua/plenary.nvim" } },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon.setup()
 
-      keymap.set(
-        "n",
-        "<leader>hm",
-        "<cmd>lua require('harpoon.mark').add_file()<cr>",
-        { desc = "Mark file with harpoon" }
-      )
-      keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-      keymap.set(
-        "n",
-        "<leader>hp",
-        "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-        { desc = "Go to previous harpoon mark" }
-      )
-    end,
-  }
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():append()
+    end)
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set("n", "<C-g>", function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set("n", "<C-t>", function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set("n", "<C-n>", function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set("n", "<C-s>", function()
+      harpoon:list():select(4)
+    end)
+  end,
+}
