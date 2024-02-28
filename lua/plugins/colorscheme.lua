f79522b570793a7b52785d4c4149c55e0ba1c47a
shivam rajput <shivamrajput769777@gmail.com>
2024-02-29 03:13:20 +0530

    refactor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,25 @@
 return {
   {
+    "sainnhe/gruvbox-material",
+    enabled = true,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard" -- soft, medium, hard
+      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "bright" -- Background of floating windows
+      vim.g.gruvbox_material_statusline_style = "material"
+      vim.g.gruvbox_material_cursor = "auto"
+
+      -- vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
+      -- vim.g.gruvbox_material_colors_override = { bg0 = "#121212" }
+      -- vim.g.gruvbox_material_better_performance = 1
+
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
     "navarasu/onedark.nvim",
     config = function(_)
       local one_dark_overrides = {
@@ -100,7 +120,7 @@ return {
         colors = one_dark_overrides,
         highlights = highlights,
       })
-      require("onedark").load()
+      -- require("onedark").load()
     end,
   },
 
