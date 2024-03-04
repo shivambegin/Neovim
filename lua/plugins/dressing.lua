76514c34b3c61556aa528b063d93431d6d8f1ad4
shivam rajput <shivamrajput769777@gmail.com>
2024-03-04 23:47:01 +0530

    dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -2,22 +2,34 @@ return {
   "stevearc/dressing.nvim",
   event = "VeryLazy",
   config = function()
-    require("dressing").setup({
+    local status_ok, dressing = pcall(require, "dressing")
+    if not status_ok then
+      return
+    end
+
+    dressing.setup({
+      win_options = {
+        winblend = 10,
+        winhighlight = "Normal:DressingInputNormalFloat,NormalFloat:DressingInputNormalFloat,FloatBorder:DressingInputFloatBorder",
+      },
       input = {
         enabled = true,
-        default_prompt = "➤ ",
-        win_options = {
-          winblend = 0,
-        },
+        default_prompt = "Input:",
+        prompt_align = "left",
+        insert_only = true,
+        start_in_insert = true,
+        border = "rounded",
+        relative = "cursor",
+        prefer_width = 40,
+        width = nil,
+        max_width = { 140, 0.9 },
+        min_width = { 20, 0.2 },
+        get_config = nil,
       },
       select = {
         enabled = true,
-        backend = { "telescope", "builtin" },
-        builtin = {
-          win_options = {
-            winblend = 0,
-          },
-        },
+        backend = { "telescope", "fzf_lua", "fzf", "nui", "builtin" },
+        trim_prompt = true,
       },
     })
   end,
