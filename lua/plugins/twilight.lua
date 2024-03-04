3ab3e1e8a43fcaf581d461364479c53bc98ad595
shivam rajput <shivamrajput769777@gmail.com>
2024-03-04 23:50:00 +0530

    twilight

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -1,27 +1,18 @@
 return -- Lua
 {
-
   "folke/twilight.nvim",
-  opts = {
-    dimming = {
-      alpha = 0.25, -- amount of dimming
+  config = function()
+    local status_ok, twilight = pcall(require, "twilight")
+    if not status_ok then
+      return
+    end
 
-      -- we try to get the foreground from the highlight groups or fallback color
-      color = { "Normal", "#ffffff" },
-      term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
-      inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
-    },
-    context = 10, -- amount of lines we will try to show around the current line
-    treesitter = true, -- use treesitter when available for the filetype
-    -- treesitter is used to automatically expand the visible text,
-    -- but you can further control the types of nodes that should always be fully expanded
-    expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
-      "function",
-      "method",
-
-      "table",
-      "if_statement",
-    },
-    exclude = {}, -- exclude these filetypes
-  },
+    twilight.setup({
+      dimming = { alpha = 0.45, color = { "Normal", "#ffffff" }, inactive = false },
+      context = 9,
+      treesitter = true,
+      expand = { "function", "method", "table", "if_statement", "element" },
+      exclude = {},
+    })
+  end,
 }
