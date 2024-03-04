ffbb66f826a5797a76ee73cac3ff54711eef05c4
shivam rajput <shivamrajput769777@gmail.com>
2024-03-05 02:25:37 +0530

    refactor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "rebelot/kanagawa.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme kanagawa")
+    end,
+  },
+
+  {
     "projekt0n/github-nvim-theme",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
@@ -8,7 +21,7 @@ return {
         -- ...
       })
 
-      vim.cmd("colorscheme github_dark_colorblind")
+      -- vim.cmd("colorscheme github_dark_colorblind")
     end,
   },
 }
