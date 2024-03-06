97cb9fb3cc1fd6511fb5d0e1ee76063df7efc93d
shivam rajput <shivamrajput769777@gmail.com>
2024-03-07 00:31:40 +0530

    disable : dropbar

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,6 +1,6 @@
 return {
   "Bekaboo/dropbar.nvim",
-  enabled = true,
+  enabled = false,
   -- optional, but required for fuzzy finder support
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
