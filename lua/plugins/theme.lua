d524eae25454de33d2b49f235ff3708ec9f9f25e
shivam rajput <shivamrajput769777@gmail.com>
2024-03-11 02:44:34 +0530

    new theme: cattpuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,7 +6,19 @@ return {
       vim.g.sonokai_transparent_background = "1"
       vim.g.sonokai_enable_italic = "1"
       vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
+      -- vim.cmd.colorscheme("sonokai")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
