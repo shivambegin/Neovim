beb9d1bef2123bf43c53087750a5d198a88cb844
shivam rajput <shivamrajput769777@gmail.com>
2024-03-14 17:27:55 +0530

    removing unnecessary themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,4 @@
 return {
-  -- Lua
-
   {
     "olivercederborg/poimandres.nvim",
     lazy = false,
@@ -10,46 +8,6 @@ return {
     end,
     init = function()
       vim.cmd("colorscheme poimandres")
-    end,
-  },
-  {
-    "2nthony/vitesse.nvim",
-    dependencies = {
-      "tjdevries/colorbuddy.nvim",
-    },
-    config = function()
-      -- vim.cmd.colorscheme("vitesse")
-    end,
-  },
-  {
-    "sainnhe/sonokai",
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_transparent_background = "1"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "andromeda"
-      -- vim.cmd.colorscheme("sonokai")
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = true,
-      })
-      -- vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      -- vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
