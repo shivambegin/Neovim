34d1ed96bfbe3cab19df8fa354ad88bd0892656c
shivam rajput <shivamrajput769777@gmail.com>
2024-03-21 02:34:05 +0530

    catppuccin theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,47 @@
 return {
   {
+
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        color_overrides = {
+          mocha = {
+            rosewater = "#ffc0b9",
+            flamingo = "#f5aba3",
+            pink = "#f592d6",
+            mauve = "#c0afff",
+            red = "#ea746c",
+            maroon = "#ff8595",
+            peach = "#fa9a6d",
+            yellow = "#ffe081",
+            green = "#99d783",
+            teal = "#47deb4",
+            sky = "#00d5ed",
+            sapphire = "#00dfce",
+            blue = "#00baee",
+            lavender = "#abbff3",
+            text = "#cccccc",
+            subtext1 = "#bbbbbb",
+            subtext0 = "#aaaaaa",
+            overlay2 = "#999999",
+            overlay1 = "#888888",
+            overlay0 = "#777777",
+            surface2 = "#666666",
+            surface1 = "#555555",
+            surface0 = "#444444",
+            base = "#202020",
+            mantle = "#222222",
+            crust = "#333333",
+          },
+        },
+      })
+      vim.cmd("colorscheme catppuccin-mocha")
+    end,
+  },
+  {
     "sam4llis/nvim-tundra",
     lazy = false,
     priority = 1000,
@@ -60,7 +102,7 @@ return {
 
       vim.g.tundra_biome = "arctic" -- 'arctic' or 'jungle'
       vim.opt.background = "dark"
-      vim.cmd("colorscheme tundra")
+      -- vim.cmd("colorscheme tundra")
     end,
   },
   {
