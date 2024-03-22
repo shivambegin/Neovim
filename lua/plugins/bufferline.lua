273af31a982dc916ebcc416eb7568312a5ff2792
shivam rajput <shivamrajput769777@gmail.com>
2024-03-23 01:49:59 +0530

    refactoring the bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,16 +4,33 @@ return {
   keys = {},
   opts = {
     options = {
-      indicator = { style = "none" },
-      buffer_close_icon = "",
-      separator_style = { "|", "|" },
+      indicator = {
+        icon = "▊",
+        style = "icon",
+      },
+      truncate_names = false,
+      modified_icon = "●",
+      color_icons = true,
+      show_buffer_icons = true,
+      show_buffer_close_icons = false,
+      separator_style = thin,
+      left_trunc_marker = "<U+F0A8>",
+      right_trunc_marker = "<U+F0A9>",
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end,
       always_show_bufferline = false,
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "Explorer",
-          text_align = "center",
-        },
+    },
+    highlights = {
+      buffer_selected = {
+        bold = true,
+        italic = true,
+        sp = "#ef8f8f",
+      },
+      tab_selected = {
+        sp = "#af6faf",
       },
     },
   },
