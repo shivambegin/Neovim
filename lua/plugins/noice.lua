69a604f6f19b0587acb187c30811619abde9b94e
shivam rajput <shivamrajput769777@gmail.com>
2024-03-24 01:09:39 +0530

    noice-notify config refactor

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -29,6 +29,12 @@ return {
         progress = {
           enabled = false,
         },
+        hover = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
+        },
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,18 +1,37 @@
 return {
   "rcarriga/nvim-notify",
-  enabled = true,
+  opts = {
+    split = true,
+    background_colour = "#000000",
+    render = "compact",
+    stages = "fade_in_slide_out",
+    top_down = true,
+  },
   keys = {
+    { "<leader>tn", "<cmd>Telescope notify<cr>", desc = "Notifications" },
+    {
+      "<leader>uN",
+      function()
+        local notify = require("notify")
+
+        local lines = {}
+        for _, notif in ipairs(notify.history()) do
+          table.insert(lines, ("%s %s: %s"):format(notif.title[1], notif.title[2], table.concat(notif.message, "\n")))
+        end
+        local buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+        vim.cmd("vsplit")
+        vim.api.nvim_win_set_buf(0, buf)
+      end,
+      desc = "Notifications",
+    },
     {
       "<leader>un",
       function()
-        require("notify").dismiss({ silent = true, pending = true })
+        return require("notify").dismiss()
       end,
-      desc = "Dismiss all Notifications",
+      desc = "Dismiss Notifications",
     },
-  },
-  opts = {
-    timeout = 5000,
-    background_colour = "#000000",
-    render = "wrapped-compact",
   },
 }
