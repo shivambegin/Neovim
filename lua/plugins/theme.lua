15a5ab242edf3de54c2dd8fb7e6c85bdc05d9460
shivam rajput <shivamrajput769777@gmail.com>
2024-03-26 03:56:54 +0530

    theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,7 +4,22 @@ return {
     -- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
     config = function()
       require("mellifluous").setup({}) -- optional, see configuration section.
-      vim.cmd("colorscheme mellifluous")
+      -- vim.cmd("colorscheme mellifluous")
+    end,
+  },
+  {
+    "m4xshen/catppuccinight.nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      custom_highlights = function(colors)
+        return {
+          VertSplit = { fg = colors.surface0 },
+        }
+      end,
+    },
+    init = function()
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
