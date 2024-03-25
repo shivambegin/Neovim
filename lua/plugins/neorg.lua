86ac3940d8406bd64769fad022464d5f1bde011b
shivam rajput <shivamrajput769777@gmail.com>
2024-03-25 23:38:41 +0530

    plugin updates

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -2,7 +2,7 @@
 
 return {
   "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
+  -- build = ":Neorg sync-parsers",
   -- tag = "*",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
