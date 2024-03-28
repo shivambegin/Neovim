1431f1f14912ce2bd087940f87c245cab7f07ea4
shivam rajput <shivamrajput769777@gmail.com>
2024-03-29 04:23:28 +0530

    enable : noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,10 +1,10 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  enabled = false,
+  enabled = true,
   dependencies = {
     "MunifTanjim/nui.nvim",
-    -- "rcarriga/nvim-notify",
+    "rcarriga/nvim-notify",
   },
   config = function()
     require("noice").setup({
