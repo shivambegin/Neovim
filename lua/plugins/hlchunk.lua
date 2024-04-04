eb4a78654fbea0d5f14a45c5229afcda1019f4c7
shivam rajput <shivamrajput769777@gmail.com>
2024-04-05 01:44:20 +0530

    hlchunk.nvim an alternative for indent blankline

--- lua/plugins/hlchunk.lua
+++ lua/plugins/hlchunk.lua
@@ -2,6 +2,54 @@ return {
   "shellRaining/hlchunk.nvim",
   event = { "UIEnter" },
   config = function()
-    require("hlchunk").setup({})
+    require("hlchunk").setup({
+      chunk = {
+        enable = true,
+        notify = true,
+        use_treesitter = true,
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "┌",
+          left_bottom = "└",
+          right_arrow = "",
+        },
+
+        style = {
+          { fg = "#806d9c" },
+          { fg = "#c21f30" }, -- this fg is used to highlight wrong chunk
+        },
+        textobject = "",
+        max_file_size = 1024 * 1024,
+        error_sign = true,
+      },
+
+      indent = {
+        enable = true,
+        use_treesitter = false,
+        chars = {
+          "│",
+        },
+        style = {
+          { fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui") },
+        },
+      },
+
+      line_num = {
+        enable = true,
+        use_treesitter = false,
+        style = "#806d9c",
+      },
+
+      blank = {
+        enable = true,
+        chars = {
+          "․",
+        },
+        style = {
+          vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"),
+        },
+      },
+    })
   end,
 }
