5e26eda3bbd45ba44e6e14643056af7d8e85b52f
shivam rajput <shivamrajput769777@gmail.com>
2024-04-06 15:07:44 +0530

    disable plugin

--- lua/plugins/whitespace.lua
+++ lua/plugins/whitespace.lua
@@ -1,6 +1,8 @@
 --Reveal whitespace characters in visual mode, similar to VSCode.
+--removing it because hlchunk provide something similar
 return {
   "mcauley-penney/visual-whitespace.nvim",
+  enabled = false,
   config = true,
   opts = {
     highlight = { link = "Visual" },
