a41629ebe417624511dfe5df92f9d641c68af16d
shivam rajput <shivamrajput769777@gmail.com>
2024-04-12 06:30:28 +0530

    cat_blue preset and icons

--- lua/plugins/intro.lua
+++ lua/plugins/intro.lua
@@ -1,7 +1,8 @@
 return {
   "OXY2DEV/intro.nvim",
   enabled = true,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("intro").setup()
+    require("intro").setup({ preset = "cat_blue" })
   end,
 }
