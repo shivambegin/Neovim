f315b2d950ffbf1192590db141111b5cd7b763ce
shivam rajput <shivamrajput769777@gmail.com>
2024-04-19 01:04:59 +0530

    plugin updates

--- lua/plugins/intro.lua
+++ lua/plugins/intro.lua
@@ -3,6 +3,11 @@ return {
   enabled = true,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("intro").setup({ preset = "cat_blue" })
+    require("intro").setup({
+      preset = {
+        name = "startify",
+        opts = { "green", "recents", "list_shade" },
+      },
+    })
   end,
 }
