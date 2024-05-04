2f8ec91caa6d1d5c1a6c91962773ac54c85220df
shivam rajput <shivamrajput769777@gmail.com>
2024-05-05 02:33:15 +0530

    dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,26 +1,12 @@
 return {
-  "nvimdev/dashboard-nvim",
-  enabled = false,
-  event = "VimEnter",
-  config = function()
-    local dashboardImage = vim.api.nvim_create_augroup("DashboardImage", { clear = true })
-    local img = require("image").from_file("/home/shivam/Pictures/Screenshots/NeoVimLogo.png")
-    vim.api.nvim_create_autocmd({ "VimEnter" }, {
-      group = dashboardImage,
-      callback = function()
-        img:render()
-        img:move(27, 1)
-      end,
-    })
-    vim.api.nvim_create_autocmd({ "BufEnter" }, {
-      group = dashboardImage,
-      callback = function()
-        img:clear()
-      end,
-    })
-
-    require("dashboard").setup({})
-    require("image").setup({})
-  end,
-  dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup({
+        theme = "hyper",
+      })
+    end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  },
 }
