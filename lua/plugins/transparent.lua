d3054fc89eb2a59169b43c46ffd55127f93e8b32
shivam rajput <shivamrajput769777@gmail.com>
2024-05-06 02:25:33 +0530

    winseprator

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -56,7 +56,7 @@ return {
           "EndOfBuffer",
           "Search",
           "Cursor",
-          "WinSeparator",
+          -- "WinSeparator",
           "LazyNormal",
         }, -- table: additional groups that should be cleared
         exclude_groups = {}, -- table: groups you don't want to clear
