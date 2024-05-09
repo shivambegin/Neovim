403ed98088608f908fb9b4117b9a8eeafa915c90
shivam rajput <shivamrajput769777@gmail.com>
2024-05-10 02:23:18 +0530

    alpha.nvim new customization

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,145 +1,33 @@
 return {
   "goolord/alpha-nvim",
   config = function()
-    local present, alpha = pcall(require, "alpha")
-
-    if not present then
-      return
-    end
-
-    local header = {
-      type = "text",
-      val = {
-        "",
-        "",
-        "█   █ █ ▄▀█ █ █ █ █▀▄▀█ ",
-        "█▄▄ █▄█ █▀█ ▀▄▀ █ █ ▀ █ ",
-        "",
-        "",
-        " L U A  +  N E O V I M ",
-        "",
-        "",
-      },
-      opts = {
-        position = "center",
-        hl = "AlphaHeader",
-      },
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    -- Set header
+    dashboard.section.header.val = {
+      " ███▄    █ ██▒   █▓ ██▓ ███▄ ▄███▓",
+      " ██ ▀█   █▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
+      "▓██  ▀█ ██▒▓██  █▒░▒██▒▓██    ▓██░",
+      "▓██▒  ▐▌██▒ ▒██ █░░░██░▒██    ▒██ ",
+      "▒██░   ▓██░  ▒▀█░  ░██░▒██▒   ░██▒",
+      "░ ▒░   ▒ ▒   ░ ▐░  ░▓  ░ ▒░   ░  ░",
+      " ░     ░     ░ ░    ░     ░      ░",
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button("o", "󰊪    File Explorer", ":lua MiniFiles.open(vim.api.nvim_buf_get_name(0), false)<CR>"),
+      dashboard.button("f", "    Find File", ":Telescope find_files<CR>"),
+      dashboard.button(
+        "s",
+        "󰑴    School Stuff",
+        ":cd ~\\Desktop\\School Stuff<CR>:lua MiniFiles.open(vim.api.nvim_buf_get_name(0), false)<CR>"
+      ),
+      dashboard.button("e", "    Edit Neovim", ":cd ~\\AppData\\Local\\nvim\\<CR>:Telescope find_files<CR>"),
+      dashboard.button("q", "    Quit", ":q<CR>"),
+    }
+    dashboard.section.footer.val = {
+      "~ " .. vim.loop.cwd() .. " ~",
     }
 
-    local handle = io.popen('fd -d 2 . $HOME"/.local/share/nvim" | head -n -2 | wc -l | tr -d "\n" ')
-    local plugins = handle:read("*a")
-    handle:close()
-
-    local thingy = io.popen('echo "$(date +%a) $(date +%d) $(date +%b)" | tr -d "\n"')
-    local date = thingy:read("*a")
-    thingy:close()
-
-    local plugin_count = {
-      type = "text",
-      -- val = "└─   " .. plugins .. " plugins in total ─┘",
-      val = {
-        "└─   " .. plugins .. " plugins in total ─┘",
-        "",
-        "",
-        "",
-      },
-      opts = {
-        position = "center",
-        hl = "AlphaHeader",
-      },
-    }
-
-    local heading = {
-      type = "text",
-      -- val = "┌─   Today is " .. date .. " ─┐",
-      val = { "", "┌─   Today is " .. date .. " ─┐" },
-      opts = {
-        position = "center",
-        hl = "AlphaHeader",
-      },
-    }
-
-    local footer = {
-      type = "text",
-      val = "~ Indeed Harship will be with ease ~",
-      opts = {
-        position = "center",
-        hl = "AlphaFooter",
-      },
-    }
-
-    local function button(sc, txt, keybind)
-      local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
-
-      local opts = {
-        position = "center",
-        text = txt,
-        shortcut = sc,
-        cursor = 5,
-        width = 24,
-        align_shortcut = "right",
-        hl_shortcut = "AlphaHeader",
-        hl = "AlphaHeader",
-      }
-      if keybind then
-        opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
-      end
-
-      return {
-        type = "button",
-        val = txt,
-        on_press = function()
-          local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
-          vim.api.nvim_feedkeys(key, "normal", false)
-        end,
-        opts = opts,
-      }
-    end
-
-    local buttons = {
-      type = "group",
-      val = {
-        -- button( "LDR h", "   Recents" , ":Telescope oldfiles<CR>"),
-        -- button( "LDR y", "   Explore" , ":Telescope file_browser<CR>"),
-        -- button( "LDR /", "   Ripgrep" , ":Telescope live_grep<CR>"),
-        button("f", "    Find file", ":cd $HOME/ | Telescope find_files<CR>"),
-        button("r", "    Recent", ":Telescope oldfiles<CR>"),
-        button("w", "    Find Word", ":Telescope live_grep<CR>"),
-        -- button( "b", "    Buffers" , ":Telescope buffers<CR>"),
-        -- button( "MRK V", "   Options" , ":execute 'normal! `V'<CR>"),
-        -- button( "MRK P", "   Plugins" , ":execute 'normal! `P'<CR>"),
-      },
-      opts = {
-        spacing = 1,
-      },
-    }
-
-    local section = {
-      header = header,
-      buttons = buttons,
-      plugin_count = plugin_count,
-      heading = heading,
-      footer = footer,
-    }
-
-    local opts = {
-      layout = {
-        { type = "padding", val = 1 },
-        section.header,
-        { type = "padding", val = 1 },
-        section.heading,
-        section.plugin_count,
-        { type = "padding", val = 1 },
-        -- section.top_bar,
-        section.buttons,
-        -- section.bot_bar,
-        { type = "padding", val = 1 },
-        section.footer,
-      },
-      opts = {
-        margin = 5,
-      },
-    }
-    alpha.setup(opts)
+    alpha.setup(dashboard.opts)
   end,
 }
