23657b6fcfb3f8f74c70d367da7fa46c46d3d833
shivam rajput <shivamrajput769777@gmail.com>
2024-05-29 01:50:03 +0530

    tokyo night theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,19 @@
 return {
-  "luisiacc/gruvbox-baby",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("gruvbox-baby")
-  end,
+  {
+    "luisiacc/gruvbox-baby",
+    enabled = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("gruvbox-baby")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { style = "moon" },
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 }
