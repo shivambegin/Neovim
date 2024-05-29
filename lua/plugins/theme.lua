755509433952b8f1a1466388bd32a8473e91a7e9
shivam rajput <shivamrajput769777@gmail.com>
2024-05-30 01:56:51 +0530

    minimal theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,19 @@
 return {
   {
+    "Yazeed1s/minimal.nvim",
+    enabled = true,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("minimal")
+    end,
+  },
+
+  {
     "luisiacc/gruvbox-baby",
     enabled = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("gruvbox-baby")
+      -- vim.cmd.colorscheme("gruvbox-baby")
     end,
   },
   {
@@ -13,7 +22,7 @@ return {
     priority = 1000,
     opts = { style = "moon" },
     config = function()
-      vim.cmd([[colorscheme tokyonight]])
+      -- vim.cmd([[colorscheme tokyonight]])
     end,
   },
 }
