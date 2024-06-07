a8b4aa072ee4c97dadec2f6af9e3c910a943bbc7
shivam rajput <shivamrajput769777@gmail.com>
2024-06-07 13:50:44 +0530

    refactor: autosession config

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,16 +1,17 @@
 return {
-    "rmagatti/auto-session",
-    config = function()
-      local auto_session = require("auto-session")
+  "rmagatti/auto-session",
+  config = function()
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
-      auto_session.setup({
-        auto_restore_enabled = false,
-        auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
-      })
-
-      local keymap = vim.keymap
-
-      keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-      keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
-    end,
-  }
+    require("auto-session").setup({
+      log_level = "error",
+      auto_restore_enabled = true,
+      auto_session_suppress_dirs = { "~/", "~/.config", "~/Downloads", "~/Documents" },
+      cwd_change_handling = {
+        post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+          require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+        end,
+      },
+    })
+  end,
+}
