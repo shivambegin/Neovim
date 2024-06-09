2ea9e2d342b0d9f899d13d3cbce500c7fde01c9f
shivam rajput <shivamrajput769777@gmail.com>
2024-06-09 07:29:48 +0530

    cattpuccin theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,23 @@
 return {
   {
     "mcchrish/zenbones.nvim",
+    enabled = false,
     dependencies = {
       "rktjmp/lush.nvim",
     },
-    lazy = false,
+    lazy = true,
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme zenwritten]])
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
 }
