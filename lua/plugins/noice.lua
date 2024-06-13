7ad0972c3af92335e34a4c00963236027e27c127
shivam rajput <shivamrajput769777@gmail.com>
2024-06-14 01:38:04 +0530

    removing lualine and fidget

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,6 +8,9 @@ return {
   },
   opts = {
     lsp = {
+      progress = {
+        enabled = false,
+      },
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
