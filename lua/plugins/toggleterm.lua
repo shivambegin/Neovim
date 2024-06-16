0173942085317c2483cec2e2fe14ba45037384fa
shivam rajput <shivamrajput769777@gmail.com>
2024-06-16 21:10:39 +0530

    config rework

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,9 +1,19 @@
+local size = function()
+  return vim.o.columns * 0.5
+end
+
 return {
   "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup({
-      vim.keymap.set({ "n", "t" }, "<A-t>", "<cmd>ToggleTerm<cr>"),
-    })
-  end,
+  keys = {
+    { "<A-t>", "<cmd>ToggleTerm size=" .. size() .. "<cr>", desc = "Toggle Terminal", mode = "n" },
+    { "<A-t>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = "t" },
+  },
+  opts = {
+    shade_terminals = true,
+    direction = "vertical",
+    float_opts = {
+      -- Hide border
+      border = "none",
+    },
+  },
 }
