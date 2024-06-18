b89520f1ae3db281d5a1047474ee25ce0f4edd55
shivam rajput <shivamrajput769777@gmail.com>
2024-06-18 20:08:58 +0530

    github theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,46 @@
 return {
-  "jesseleite/nvim-noirbuddy",
-  dependencies = {
-    { "tjdevries/colorbuddy.nvim" },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+          hide_nc_statusline = false,
+          hide_end_of_buffer = false,
+          inverse = {
+            match_paren = true,
+            -- visual = true,
+            search = true,
+          },
+          darken = {
+            sidebars = {
+              enabled = false,
+            },
+          },
+        },
+        groups = {
+          all = {
+            ["@label.jsonc"] = { link = "@label.json" },
+          },
+        },
+      })
+
+      vim.g.github_theme_debug = true
+
+      vim.cmd("colorscheme github_dark_dimmed")
+    end,
   },
-  lazy = false,
-  priority = 1000,
-  opts = {
-    preset = "minimal",
+  {
+    "jesseleite/nvim-noirbuddy",
+    dependencies = {
+      { "tjdevries/colorbuddy.nvim" },
+    },
+    lazy = true,
+    priority = 1000,
+    opts = {
+      preset = "minimal",
+    },
   },
 }
