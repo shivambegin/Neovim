36db9a753c46016c93f8e5e6efe7ecba6e5228c9
shivam rajput <shivamrajput769777@gmail.com>
2024-06-19 01:21:19 +0530

    molokai theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,15 @@
 return {
   {
+    "tomasr/molokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme molokai")
+    end,
+  },
+  {
     "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("github-theme").setup({
