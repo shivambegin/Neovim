52dfff39f1734adbd1715db19a0a60c595b4f666
shivam rajput <shivamrajput769777@gmail.com>
2024-06-22 02:38:53 +0530

    feature: persistant theme switcher

---
+++ lua/plugins/themery.lua
@@ -0,0 +1,13 @@
+--theme switcher
+return {
+  "zaldih/themery.nvim",
+  config = function()
+    -- Minimal config
+    require("themery").setup({
+      themes = { "gruvbox", "ayu" }, -- Your list of installed colorschemes
+      themeConfigFile = "~/.config/nvim/lua/config/theme-select.lua", -- Described below
+      livePreview = true, -- Apply theme while browsing. Default to true.
+    })
+    vim.keymap.set("n", "<leader>sc", "<cmd>Themery<CR>", { desc = "theme selector" })
+  end,
+}
