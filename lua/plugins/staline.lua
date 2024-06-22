7f4395f18267657bf002fe6f951755931788bc60
shivam rajput <shivamrajput769777@gmail.com>
2024-06-23 02:50:28 +0530

    minor changes

--- lua/plugins/staline.lua
+++ lua/plugins/staline.lua
@@ -5,9 +5,9 @@ return {
   config = function()
     require("staline").setup({
       sections = {
-        left = { "  ", "mode", " ", "branch", " ", "lsp" },
+        left = { "mode", " ", "branch", " ", "lsp" },
         mid = { "lsp_name" },
-        right = { "cool_symbol", "file_name" },
+        right = { "Broken", "cool_symbol", "file_size" },
       },
       mode_colors = {
         i = "#d4be98",
@@ -18,10 +18,10 @@ return {
       defaults = {
         true_colors = true,
         full_path = false,
-        branch_symbol = " ",
+        branch_symbol = " ",
         expand_null_ls = true, -- This expands out all the null-ls sources to be shown
         lsp_client_character_length = 1000, -- Shorten LSP client names.
-        cool_symbol = "󰣇 ", -- Change this to override default OS icon.
+        cool_symbol = "", -- Change this to override default OS icon.
         font_active = "none", -- "bold", "italic", "bold,italic", etc
       },
     })
