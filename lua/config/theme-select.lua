9d36bdf0c187a1aa03132a9e9ffef6a0b11dbc98
shivam rajput <shivamrajput769777@gmail.com>
2024-06-23 22:25:32 +0530

    plugin updates

--- lua/config/theme-select.lua
+++ lua/config/theme-select.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme sonokai")
-vim.g.theme_id = 6
+vim.cmd("colorscheme molokai")
+vim.g.theme_id = 4
 -- end themery block
