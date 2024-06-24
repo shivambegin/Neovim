b7dfbd7429e5f15820833bd921a242cd647246f7
shivam rajput <shivamrajput769777@gmail.com>
2024-06-24 15:50:49 +0530

    github theme in themery

--- lua/config/theme-select.lua
+++ lua/config/theme-select.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme poimandres")
-vim.g.theme_id = 8
+vim.cmd("colorscheme github_dark_default")
+vim.g.theme_id = 10
 -- end themery block
