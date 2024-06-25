cb75016905d063462e0f6477096dd9329cbe8f3c
shivam rajput <shivamrajput769777@gmail.com>
2024-06-25 22:36:01 +0530

    rainbow delimeters for rainbow parenthisis

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,31 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  config = function()
+    local rainbow_delimiters = require("rainbow-delimiters")
+    require("rainbow-delimiters.setup").setup({
+      {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        priority = {
+          [""] = 110,
+          lua = 210,
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      },
+    })
+  end,
+}
