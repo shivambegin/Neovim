027145f85b4cd97fe511ad725ad6f674a7e2ec75
shivam rajput <shivamrajput769777@gmail.com>
2024-06-27 01:43:52 +0530

    fix : flash.nvim keymaps

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,6 +1,7 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
+  ---@type Flash.Config
   opts = {},
   keys = {
     {
@@ -18,6 +19,30 @@ return {
         require("flash").treesitter()
       end,
       desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
     },
   },
 }
