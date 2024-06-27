99a367b3effad2169a732c0d523657bba9405620
shivam rajput <shivamrajput769777@gmail.com>
2024-06-27 22:06:16 +0530

    transparent lualine background

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,9 +7,11 @@ return {
   lazy = false,
   event = { "BufReadPost", "BufNewFile", "VeryLazy" },
   config = function()
+    local auto_theme_custom = require("lualine.themes.auto")
+    auto_theme_custom.normal.c.bg = "none"
     require("lualine").setup({
       options = {
-        theme = "poimandres",
+        theme = auto_theme_custom,
         globalstatus = true,
         icons_enabled = true,
         component_separators = { left = "|", right = "|" },
