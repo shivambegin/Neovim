8efdfc2a122b432653d56bcf40bdc3143f5e9bdf
shivam rajput <shivamrajput769777@gmail.com>
2024-07-01 15:15:02 +0530

    changing icons for diagnostics in signcoloumn

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -68,10 +68,15 @@ return {
 
     -- Change the Diagnostic symbols in the sign column (gutter)
     -- (not in youtube nvim video)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    local signs = {
+      Error = "🔥",
+      Warn = "😤",
+      Info = "🤔",
+      Hint = "🐼",
+    }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
     end
 
     -- configure matlab server
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -10,33 +10,5 @@ return {
         enable = false,
       },
     })
-
-    -- error lens
-    vim.fn.sign_define({
-      {
-        name = "DiagnosticSignError",
-        text = "",
-        texthl = "DiagnosticSignError",
-        linehl = "ErrorLine",
-      },
-      {
-        name = "DiagnosticSignWarn",
-        text = "",
-        texthl = "DiagnosticSignWarn",
-        linehl = "WarningLine",
-      },
-      {
-        name = "DiagnosticSignInfo",
-        text = "",
-        texthl = "DiagnosticSignInfo",
-        linehl = "InfoLine",
-      },
-      {
-        name = "DiagnosticSignHint",
-        text = "",
-        texthl = "DiagnosticSignHint",
-        linehl = "HintLine",
-      },
-    })
   end,
 }
