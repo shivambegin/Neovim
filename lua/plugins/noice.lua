150e7362fa750625f328eee13115fb786657ae87
shivam rajput <shivamrajput769777@gmail.com>
2024-07-07 21:48:05 +0530

    noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,51 +2,65 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  enabled = false,
+  enabled = true,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "MunifTanjim/nui.nvim",
   },
-  opts = {
-    lsp = {
-      progress = {
-        enabled = false,
+  config = function()
+    require("noice").setup({
+      lsp = {
+        progress = {
+          enabled = false,
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+
+        signature = {
+          enabled = false,
+          auto_open = {
+            enabled = false,
+            trigger = false, -- Automatically show signature help when typing a trigger character from the LSP
+            luasnip = false, -- Will open signature help when jumping to Luasnip insert nodes
+            throttle = 50, -- Debounce lsp signature help request by 50ms
+          },
+          view = nil, -- when nil, use defaults from documentation
+          opts = {}, -- merged with defaults from documentation
+        },
       },
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
       },
-    },
-    routes = {
-      {
-        filter = {
-          event = "msg_show",
-          any = {
-            { find = "%d+L, %d+B" },
-            { find = "; after #%d+" },
-            { find = "; before #%d+" },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = true,
+      },
+      cmdline = {
+        view = "cmdline",
+      },
+      views = {
+        mini = {
+          win_options = {
+            winblend = 0,
           },
         },
-        view = "mini",
       },
-    },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-      inc_rename = true,
-      lsp_doc_border = true,
-    },
-    cmdline = {
-      view = "cmdline",
-    },
-    views = {
-      mini = {
-        win_options = {
-          winblend = 0,
-        },
-      },
-    },
-  },
+    })
+  end,
 }
