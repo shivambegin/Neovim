1fe95d6c7464f97249cd1d8574fb1988acf3d181
shivam rajput <shivamrajput769777@gmail.com>
2024-07-08 03:46:14 +0530

    oil.nvim opens in floating window

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,7 +6,11 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("oil").setup({
-      vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+      float = {
+        max_height = 20,
+        max_width = 60,
+      },
     })
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
   end,
 }
