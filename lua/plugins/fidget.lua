438e644313344caa579ba39b2cbb9038f73bd50b
shivam rajput <shivamrajput769777@gmail.com>
2024-07-07 21:44:43 +0530

    fidget config

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,6 +1,6 @@
 return {
   "j-hui/fidget.nvim",
-  enabled = false,
+  enabled = true,
   tag = "legacy",
   event = "LspAttach",
   opts = {
@@ -9,6 +9,14 @@ return {
     },
     window = {
       blend = 0,
+      border = "none", -- Border around the notification window
+      zindex = 45, -- Stacking priority of the notification window
+      max_width = 0, -- Maximum width of the notification window
+      max_height = 0, -- Maximum height of the notification window
+      x_padding = 1, -- Padding from right edge of window boundary
+      y_padding = 0, -- Padding from bottom edge of window boundary
+      align = "bottom", -- How to align the notification window
+      relative = "editor", -- What the notification window position is relative to
     },
   },
 }
