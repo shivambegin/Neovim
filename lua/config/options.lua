4c078b043f2e09c5695a8f5d2c8a175a2e4df368
shivam rajput <shivamrajput769777@gmail.com>
2024-07-10 15:41:51 +0530

    statusline bg and cmdheight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,7 @@ vim.g.maplocalleader = " "
 
 vim.opt.backup = false -- creates a backup file
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
+vim.opt.cmdheight = 0 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
 -- vim.opt.fileencoding = "utf-8" -- the encoding written to a file
