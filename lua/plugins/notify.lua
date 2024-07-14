9dd2e90c3ca14e69ec03a5dfbf654d05fcd5293e
shivam rajput <shivamrajput769777@gmail.com>
2024-07-14 09:24:34 +0530

    plugin updates

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -13,10 +13,10 @@ return {
   opts = {
     timeout = "3000",
     max_height = function()
-      return math.floor(vim.o.lines * 0.35)
+      return math.floor(vim.o.lines * 0.75)
     end,
     max_width = function()
-      return math.floor(vim.o.columns * 0.35)
+      return math.floor(vim.o.columns * 0.75)
     end,
     render = "compact",
     background_colour = "#000000",
