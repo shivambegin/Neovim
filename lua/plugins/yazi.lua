9eb07d0baf714acf91a91c81e5eccda84eeb3c73
shivam rajput <shivamrajput769777@gmail.com>
2024-07-15 13:27:35 +0530

    yazi support for nvim

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,4 +1,3 @@
----@type LazySpec
 return {
   "mikavilpas/yazi.nvim",
   event = "VeryLazy",
@@ -12,7 +11,6 @@ return {
       desc = "Open the file manager in nvim's working directory",
     },
   },
-  ---@type YaziConfig
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
