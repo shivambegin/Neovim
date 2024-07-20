6b4ebd5c27e052c6669e499b13e085d519c535a1
shivam rajput <shivamrajput769777@gmail.com>
2024-07-20 14:13:17 +0530

    cattpuccin theme: overrites some colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,23 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("catppuccin-mocha")
-  end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      color_overrides = {
+        mocha = {
+          base = "#11111B",
+          mantle = "#11111B",
+        },
+      },
+      custom_highlights = function(colors)
+        return {
+          VertSplit = { fg = colors.surface0 },
+        }
+      end,
+    },
+    init = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
