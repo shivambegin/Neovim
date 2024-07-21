0825190bc838f2ed8ecab98e18d2625d3cbe4fd8
shivam rajput <shivamrajput769777@gmail.com>
2024-07-21 21:05:08 +0530

    ui changes: heirline and bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,21 +1,19 @@
 return {
   "akinsho/bufferline.nvim",
-  event = "VeryLazy",
+  event = { "BufReadPre", "BufNewFile" },
   enabled = false,
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   opts = {
     options = {
       debug = { logging = true },
-      style_preset = "minimal",
       mode = "buffers",
+      indicator = { icon = "▍", style = "icon" },
       sort_by = "insert_after_current",
-      separator_style = "|",
       move_wraps_at_ends = true,
       right_mouse_command = "vert sbuffer %d",
       show_close_icon = false,
       show_buffer_close_icons = false,
-      indicator = { style = "underline" },
       diagnostics = "nvim_lsp",
       diagnostics_indicator = function(count, level)
         level = level:match("warn") and "warn" or level
