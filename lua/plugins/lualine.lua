7e13b47490654d4b6689e1b98b85ca705a9feaeb
shivam rajput <shivamrajput769777@gmail.com>
2024-07-25 02:29:49 +0530

    lualine config changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,7 +13,7 @@ return {
       section_separators = { left = "", right = "" },
       globalstatus = true,
       refresh = {
-        statusline = 100,
+        statusline = 1000,
       },
     },
     sections = {
@@ -22,17 +22,64 @@ return {
       },
       lualine_b = { "none" },
       lualine_c = {
-        { "fancy_cwd", substitute_home = true },
-        { "fancy_diff" },
+        {
+          "filename",
+          file_status = true,
+          newfile_status = false,
+          path = 1,
+          -- Shortens path to leave 40 spaces in the window
+          shorting_target = 40,
+          symbols = {
+            modified = "(modified)",
+            readonly = "(readonly)",
+            unnamed = "[No Name]",
+            newfile = "[New]",
+          },
+        },
+        {
+          "diff",
+          symbols = {
+            added = " ",
+            modified = " ",
+            removed = " ",
+          },
+        },
       },
       lualine_x = {
-        { "fancy_macro" },
-        { "fancy_diagnostics" },
+        {
+          "filetype",
+          colored = true,
+          padding = { left = 0, right = 1 },
+          icon = { align = "right" },
+        },
+        {
+          "diagnostics",
+          sections = { "error", "warn", "info", "hint" },
+          diagnostics_color = {
+            -- Same values as the general color option can be used here.
+            error = "DiagnosticError",
+            warn = "DiagnosticWarn",
+            info = "DiagnosticInfo",
+            hint = "DiagnosticHint",
+          },
+          symbols = { error = "Err ", warn = "Warn ", info = "Info ", hint = "Hint " },
+          colored = true,
+          update_in_insert = false,
+          always_visible = false,
+        },
       },
       lualine_y = { "none" },
       lualine_z = {
         { "fancy_lsp_servers" },
       },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
     },
   },
 }
