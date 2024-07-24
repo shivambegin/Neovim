6f47b05fb7214041ea4835e04c481846ffcadf6f
shivam rajput <shivamrajput769777@gmail.com>
2024-07-25 01:55:11 +0530

    ui changes: neotree and neoscroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -4,10 +4,25 @@ return {
   event = { "CursorHold", "CursorHoldI" },
   config = function()
     require("neoscroll").setup({
-      stop_eof = true,
-      easing_function = "sine",
-      hide_cursor = true,
-      cursor_scrolls_alone = true,
+      mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+        "<C-u>",
+        "<C-d>",
+        "<C-b>",
+        "<C-f>",
+        "<C-y>",
+        "<C-e>",
+        "zt",
+        "zz",
+        "zb",
+      },
+      hide_cursor = true, -- Hide cursor while scrolling
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      easing = "linear", -- Default easing function
+      pre_hook = nil, -- Function to run before the scrolling animation starts
+      post_hook = nil, -- Function to run after the scrolling animation ends
+      performance_mode = false, -- Disable "Performance Mode" on all buffers.
     })
   end,
 }
