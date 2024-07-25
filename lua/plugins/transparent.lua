b362c73331f18b5818331a55d1a8c27b57555bff
shivam rajput <shivamrajput769777@gmail.com>
2024-07-26 04:25:43 +0530

    transparant dropbar

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,10 +1,11 @@
 return {
   {
     "xiyaowong/transparent.nvim",
-    enabled = false,
+    enabled = true,
     config = function()
       require("transparent").clear_prefix("WinBar")
       require("transparent").clear_prefix("ToggleTerm1")
+      require("transparent").clear_prefix("DropBar")
       -- require("transparent").clear_prefix("Normal")
       require("transparent").clear_prefix("Navic")
       -- require("transparent").clear_prefix("Mason")
