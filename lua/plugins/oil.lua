3a196d3bc81280d6d3d4258a36c07cbea7ae6c8d
shivam rajput <shivamrajput769777@gmail.com>
2024-07-25 18:51:43 +0530

    plugin updates

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -11,6 +11,6 @@ return {
         max_width = 60,
       },
     })
-    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
