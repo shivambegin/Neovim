cc16efa75e24330514c0a434da2f2fa9e3a5e9fc
shivam rajput <shivamrajput769777@gmail.com>
2024-07-26 18:37:11 +0530

    removing goto diagnostic keymap because its default form nvim 0.10

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -50,12 +50,6 @@ return {
         opts.desc = "Show line diagnostics"
         keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-        opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-        opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
         opts.desc = "Show documentation for what is under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
