ffd59422e68b7abb5615f56b51efd1b81eebc1aa
shivam rajput <shivamrajput769777@gmail.com>
2024-08-02 08:04:01 +0530

    feat: incline nvim and disabling cursorline

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -3,66 +3,52 @@ return {
   enabled = true,
   event = { "BufReadPost", "BufAdd", "BufNewFile" },
   config = function()
-    local function get_diagnostic_label(props)
-      local icons = require("config.icons")
-      local label = {}
-      for severity, icon in pairs(icons.diagnostics) do
-        local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-        if n > 0 then
-          table.insert(label, { icon .. "" .. n .. " ", group = "DiagnosticSign" .. severity })
-        end
-      end
-      return label
-    end
-
-    local function get_git_diff(props)
-      local icons = { removed = "", changed = "", added = "" }
-      local labels = {}
-      local success, signs = pcall(vim.api.nvim_buf_get_var, props.buf, "gitsigns_status_dict")
-      if success then
-        for name, icon in pairs(icons) do
-          if tonumber(signs[name]) and signs[name] > 0 then
-            table.insert(labels, { icon .. " " .. signs[name] .. " ", group = "Diff" .. name })
-          end
-        end
-        return labels
-      else
-        for name, icon in pairs(icons) do
-          if tonumber(signs[name]) and signs[name] > 0 then
-            table.insert(labels, { icon .. " " .. signs[name] .. " ", group = "Diff" .. name })
-          end
-        end
-        return labels
-      end
-    end
-
     require("incline").setup({
-      hide = {
-        cursorline = true,
-        focused_win = false,
-        only_win = false,
-      },
-      window = {
-        margin = {
-          horizontal = 1,
-          vertical = 2,
-        },
-        placement = {
-          horizontal = "right",
-          vertical = "top",
-        },
-      },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
         local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-        local modified = vim.api.nvim_get_option_value("modified", { buf = props.bufnr }) and "bold,italic" or "normal"
+        local modified = vim.bo[props.buf].modified and "bold,italic" or "bold"
+
+        local function get_git_diff()
+          local icons = { removed = " ", changed = " ", added = " " }
+          icons["changed"] = icons.modified
+          local signs = vim.b[props.buf].gitsigns_status_dict
+          local labels = {}
+          if signs == nil then
+            return labels
+          end
+          for name, icon in pairs(icons) do
+            if tonumber(signs[name]) and signs[name] > 0 then
+              table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
+            end
+          end
+          if #labels > 0 then
+            table.insert(labels, { " " })
+          end
+          return labels
+        end
+        local function get_diagnostic_label()
+          local icons = { error = "", warn = "", info = "", hint = "" }
+          local label = {}
+
+          for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if n > 0 then
+              table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+            end
+          end
+          if #label > 0 then
+            table.insert(label, { " " })
+          end
+          return label
+        end
 
         local buffer = {
-          { get_git_diff(props) },
-          { get_diagnostic_label(props) },
-          { ft_icon, guifg = ft_color },
-          { " " },
-          { filename, gui = modified },
+          { get_diagnostic_label() },
+          { get_git_diff() },
+          { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+          { filename .. " ", gui = modified },
+          { "  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
         }
         return buffer
       end,
