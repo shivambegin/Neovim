360af7bddde2cc542478ae5aa2b02797652622ea
shivambegin <shivamrajput769777@gmail.com>
2024-08-04 04:56:19 +0530

    keymap to choose colorscheme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,6 +21,7 @@ return {
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
       vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sc", builtin.colorscheme, { desc = "[S]earch [C]olorscheme" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       telescope.setup({
