7e85f1a4d78efb910734f6aab4bac0d4aad5cc04
shivambegin <shivamrajput769777@gmail.com>
2024-08-05 12:39:45 +0530

    plugin updates

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
+  enabled = false,
   dependencies = { { "nvim-lua/plenary.nvim" } },
   config = function()
     local harpoon = require("harpoon")
--- lua/plugins/headlines.lua
+++ lua/plugins/headlines.lua
@@ -1,18 +1,8 @@
 return {
   "lukas-reineke/headlines.nvim",
   dependencies = "nvim-treesitter/nvim-treesitter",
+  enbaled = false,
   config = function()
-    require("headlines").setup({
-      quarto = {
-        query = vim.treesitter.query.parse(
-          "markdown",
-          [[
-                (fenced_code_block) @codeblock
-            ]]
-        ),
-        codeblock_highlight = "CodeBlock",
-        treesitter_language = "markdown",
-      },
-    })
+    require("headlines").setup({})
   end,
 }
