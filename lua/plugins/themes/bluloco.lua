12c12f8d8636375a1301335916bfb31cd512ccb2
shivambegin <shivamrajput769777@gmail.com>
2024-08-11 11:32:41 +0530

    more colorschemes

---
+++ lua/plugins/themes/bluloco.lua
@@ -0,0 +1,9 @@
+return {
+  "uloco/bluloco.nvim",
+  lazy = false,
+  priority = 1000,
+  dependencies = { "rktjmp/lush.nvim" },
+  config = function()
+    -- your optional config goes here, see below.
+  end,
+}
