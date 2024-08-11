b229712ecea999ec40ad73dba733c97232f8d86e
shivambegin <shivamrajput769777@gmail.com>
2024-08-11 06:02:23 +0530

    autocmd colorscheme to work with neovim

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,18 +1,21 @@
 -- don't auto comment new line
 vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
+-- to make border as same as neovim ColorScheme
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
   callback = function()
     local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
     if not normal.bg then
       return
     end
+    io.write(string.format("\027Ptmux;\027\027]11;#%06x\007\027\\", normal.bg))
     io.write(string.format("\027]11;#%06x\027\\", normal.bg))
   end,
 })
 
 vim.api.nvim_create_autocmd("UILeave", {
   callback = function()
+    io.write("\027Ptmux;\027\027]111;\007\027\\")
     io.write("\027]111\027\\")
   end,
 })
