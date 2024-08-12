f2755db79cf4b7a62cbd5cea551653155d9a4d91
shivambegin <shivamrajput769777@gmail.com>
2024-08-12 12:38:50 +0530

    evil-line with vim icon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -97,7 +97,7 @@ return {
     ins_left({
       -- mode component
       function()
-        return ""
+        return ""
       end,
       color = function()
         -- auto change color according to neovims mode
