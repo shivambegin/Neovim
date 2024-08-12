5b056a66bc8d9d0739d51caed43f88283e9df9de
shivambegin <shivamrajput769777@gmail.com>
2024-08-12 13:03:47 +0530

    colorschemes

---
+++ lua/plugins/themes/gruvbox-material.lua
@@ -0,0 +1,8 @@
+return {
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_enable_italic = true
+  end,
+}
---
+++ lua/plugins/themes/nigthfox.lua
@@ -0,0 +1,6 @@
+return {
+  "EdenEast/nightfox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function() end,
+}
