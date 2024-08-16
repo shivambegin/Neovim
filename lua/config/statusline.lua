9e08b88e870afe2c9ffb8f2f4221d61280c4728c
shivam rajput <shivamrajput769777@gmail.com>
2024-08-16 15:55:42 +0530

    feat: show full mode name in statusline(native)

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -26,9 +26,32 @@ local function get_git_diff(type)
   return 0
 end
 
+local modes = {
+  ["n"] = "NORMAL",
+  ["no"] = "NORMAL",
+  ["v"] = "VISUAL",
+  ["V"] = "VISUAL LINE",
+  [""] = "VISUAL BLOCK",
+  ["s"] = "SELECT",
+  ["S"] = "SELECT LINE",
+  [""] = "SELECT BLOCK",
+  ["i"] = "INSERT",
+  ["ic"] = "INSERT",
+  ["R"] = "REPLACE",
+  ["Rv"] = "VISUAL REPLACE",
+  ["c"] = "COMMAND",
+  ["cv"] = "VIM EX",
+  ["ce"] = "EX",
+  ["r"] = "PROMPT",
+  ["rm"] = "MOAR",
+  ["r?"] = "CONFIRM",
+  ["!"] = "SHELL",
+  ["t"] = "TERMINAL",
+}
 --- @return string
 local function mode()
-  return string.format("%%#StatusLineMode# %s %%*", vim.api.nvim_get_mode().mode)
+  local current_mode = vim.api.nvim_get_mode().mode
+  return string.format(" %s ", modes[current_mode]):upper()
 end
 
 --- @return string
--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -2,7 +2,7 @@ return {
   "rebelot/heirline.nvim",
   dependencies = { "zeioth/heirline-components.nvim" },
   event = { "BufReadPost", "BufAdd", "BufNewFile" },
-  enabled = true,
+  enabled = false,
   opts = function()
     local lib = require("heirline-components.all")
     return {
