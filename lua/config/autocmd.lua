1dd9b0567bcc460e97bb2b98bbfcb7b12a9af09d
shivam rajput <shivamrajput769777@gmail.com>
2024-08-17 23:24:16 +0530

    wip

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,18 +1,21 @@
 -- toggle relative number on the basis of mode
-local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {})
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter", "CmdlineLeave" }, {
-  group = numbertoggle,
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = augroup,
   callback = function()
-    if vim.opt.number and vim.api.nvim_get_mode() ~= "i" then
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
       vim.opt.relativenumber = true
     end
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave", "CmdlineEnter" }, {
-  group = numbertoggle,
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = augroup,
   callback = function()
-    if vim.opt.number then
+    if vim.o.nu then
       vim.opt.relativenumber = false
       vim.cmd("redraw")
     end
