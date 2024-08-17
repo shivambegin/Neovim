f6cd7412c4b6050640cf88f5cf072b773aedd165
shivam rajput <shivamrajput769777@gmail.com>
2024-08-17 20:58:47 +0530

    wip

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -2,6 +2,26 @@
 -- vim.api.nvim_create_autocmd("InsertEnter", { command = [[set norelativenumber]] })
 -- vim.api.nvim_create_autocmd("InsertLeave", { command = [[set relativenumber]] })
 
+-- toggle relative number on the basis of mode
+local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {})
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter", "CmdlineLeave" }, {
+  group = numbertoggle,
+  callback = function()
+    if vim.opt.number and vim.api.nvim_get_mode() ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave", "CmdlineEnter" }, {
+  group = numbertoggle,
+  callback = function()
+    if vim.opt.number then
+      vim.opt.relativenumber = false
+      vim.cmd("redraw")
+    end
+  end,
+})
 -- don't auto comment new line
 vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
