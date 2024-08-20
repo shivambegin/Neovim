b8b4ce14bdcb53ff239538a04568612299bd7b3f
shivam rajput <shivamrajput769777@gmail.com>
2024-08-20 17:43:35 +0530

    wip

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,15 +7,6 @@ return {
       require("mini.surround").setup({})
       require("mini.pairs").setup({}) --alternative for nvim-autopairs
       require("mini.move").setup({})
-      -- require("mini.cursorword").setup() -- alternative for cursorline
-      -- require("mini.tabline").setup()
-      local statusline = require("mini.statusline")
-      -- statusline.setup({ use_icons = true })
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return "%2l:%-2v"
-      end
-
       require("mini.bracketed").setup({
         file = { suffix = "" },
         window = { suffix = "" },
