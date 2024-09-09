631716ae432020a4c157ee7faa25cf4919fee4b2
shivam rajput <shivamrajput769777@gmail.com>
2024-09-09 15:40:33 +0530

    wip

---
+++ lua/plugins/themes/defaultplus.lua
@@ -0,0 +1,8 @@
+return {
+  "ChristianChiarulli/defaultplus",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd.colorscheme("defaultplus")
+  end,
+}
