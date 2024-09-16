9ff66a91b86373510649d7e3a051a52783f14d49
shivam rajput <shivamrajput769777@gmail.com>
2024-09-17 02:38:01 +0530

    wip

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,19 @@
 return {
   "stevearc/oil.nvim",
   opts = {
+    columns = {
+      "icon",
+    },
+    win_options = {
+      signcolumn = "auto",
+    },
+    lsp_file_methods = {
+      autosave_changes = true,
+    },
+    view_options = {
+      show_hidden = false,
+      natural_order = true,
+    },
     float = {
       max_height = 20,
       max_width = 60,
