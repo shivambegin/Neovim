abbd003069eb5020d9dbc222fa11e0f3b262dc94
shivam rajput <shivamrajput769777@gmail.com>
2024-09-17 02:17:01 +0530

    wip

---
+++ lua/plugins/everybody_wants_that_line.lua
@@ -0,0 +1,58 @@
+return {
+  "strash/everybody-wants-that-line.nvim",
+  config = function()
+    -- You can omit the config table
+    require("everybody-wants-that-line").setup()
+
+    -- or you can add it
+    require("everybody-wants-that-line").setup({
+      buffer = {
+        enabled = true,
+        prefix = "Bufnr :",
+        -- Placeholder before buffer number, e.g. "00001".
+        -- If you don't want additional symbols to be displayed,
+        -- set `symbol = ""` or `max_symbols = 0`.
+        symbol = "",
+        -- Maximum number of symbols including buffer number.
+        max_symbols = 5,
+      },
+      diagnostics = {
+        enabled = true,
+      },
+      quickfix_list = {
+        enabled = true,
+      },
+      git_status = {
+        enabled = true,
+      },
+      filepath = {
+        enabled = true,
+        -- `path` can be one of these:
+        -- "tail" - file name only
+        -- "relative" - relative to working directory
+        -- "full" - full path to the file
+        path = "relative",
+        -- If `true` a path will be shortened, e.g. "/a/b/c/filename.lua".
+        -- It only works if `path` is "relative" or "full".
+        shorten = false,
+      },
+      filesize = {
+        enabled = true,
+        -- `metric` can be:
+        -- "decimal" - 1000 bytes == 1 kilobyte
+        -- "binary" - 1024 bytes == 1 kibibyte
+        metric = "binary",
+      },
+      ruller = {
+        enabled = false,
+      },
+      -- Filename is a separate widget that is located
+      -- in the upper right corner of each open window.
+      filename = {
+        enabled = false,
+      },
+      -- Separator between components, e.g. " ... │ ... │ ... "
+      separator = "│",
+    })
+  end,
+}
