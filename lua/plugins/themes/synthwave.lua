64e67f9ff300c61c55da690b8ccf934f4bac168e
shivambegin <shivamrajputcse@gmail.com>
2024-10-06 02:38:19 +0530

    wip

--- lua/plugins/themes/synthwave.lua
+++ lua/plugins/themes/synthwave.lua
@@ -1,6 +1,7 @@
 return {
   "samharju/synthweave.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  enabled = false,
   priority = 1000,
   config = function()
     -- vim.cmd.colorscheme("synthweave")
