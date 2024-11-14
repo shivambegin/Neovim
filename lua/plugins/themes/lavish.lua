9c6d309ee299c9e830cfb12b050b72afcc0f9102
shivambegin <shivamrajputcse@gmail.com>
2024-11-14 18:57:20 +0530

    wip

--- lua/plugins/themes/lavish.lua
+++ lua/plugins/themes/lavish.lua
@@ -1,6 +1,6 @@
 return {
   "ferdinandrau/lavish.nvim",
-  enabled = true,
+  enabled = false,
   priority = 1000,
   config = function()
     require("lavish").apply()
---
+++ lua/plugins/themes/monoglow.lua
@@ -0,0 +1,9 @@
+return {
+  "wnkz/monoglow.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd([[colorscheme monoglow-void]])
+  end,
+}
